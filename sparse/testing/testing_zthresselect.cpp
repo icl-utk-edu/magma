76f81c8d746b919cff06a717f06155cc2757fabe
Hartwig Anzt <hanzt@icl.utk.edu>
2018-02-01 16:07:28 +0100

    output formatting

--- sparse/testing/testing_zthresselect.cpp
+++ sparse/testing/testing_zthresselect.cpp
@@ -38,35 +38,38 @@ int main(  int argc, char** argv )
     real_Double_t start, end;
     
     double thrs;
-    int m = 10000;
-    int n = 100;
-    int count = 0;
-    magmaDoubleComplex *val, *d_val;
-    TESTING_CHECK(magma_zmalloc_cpu(&val, m));
-    TESTING_CHECK(magma_zmalloc(&d_val, m));
-    // fill the values with random numbers
-    for (int z=0; z<m; z++){
-        val[z] = MAGMA_Z_MAKE((double)(rand()%m)/(double)m, 0.0);        
-    }
-    
-    // copy over
-    magma_zsetvector( m, val, 1, d_val, 1, queue );
-    
-    start = magma_sync_wtime( queue );
-    for(int i=0; i<10; i++)
-        TESTING_CHECK(magma_zthrsholdselect(m, n, d_val, &thrs, queue));
-    end = magma_sync_wtime( queue );
-    printf( " > %.2e seconds.\n", (end-start)/10 );
-    
-    for(int z=0; z<m; z++) {
-        if (MAGMA_Z_ABS(val[z])<thrs) {
-            count++;    
+    for( int m = 1000; m<10000001; m++) {
+        int n = 100;
+        int count = 0;
+        magmaDoubleComplex *val, *d_val;
+        TESTING_CHECK(magma_zmalloc_cpu(&val, m));
+        TESTING_CHECK(magma_zmalloc(&d_val, m));
+        // fill the values with random numbers
+        for (int z=0; z<m; z++){
+            val[z] = MAGMA_Z_MAKE((double)(rand()%m)/(double)m, 0.0);        
         }
+        
+        // copy over
+        magma_zsetvector( m, val, 1, d_val, 1, queue );
+        
+        start = magma_sync_wtime( queue );
+        for(int i=0; i<10; i++)
+            TESTING_CHECK(magma_zthrsholdselect(m, n, d_val, &thrs, queue));
+        end = magma_sync_wtime( queue );
+        
+        
+        for(int z=0; z<m; z++) {
+            if (MAGMA_Z_ABS(val[z])<thrs) {
+                count++;    
+            }
+        }
+                
+        magma_free(d_val);
+        magma_free_cpu(val);
+        printf("%% m n thrs count sec\n");
+
+        printf( " %10d  %10d  %.8e  %10d %.4e\n", n, m, thrs, count, (end-start)/10 );
     }
-    printf("selecting threshold for %d of %d elements: threshold selected: %.8e -> exact to %d elements.\n", n, m, thrs, count);
-    
-    magma_free(d_val);
-    magma_free_cpu(val);
     
     magma_queue_destroy( queue );
     TESTING_CHECK( magma_finalize() );
