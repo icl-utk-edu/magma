8ce999cfe271211e640bd8911fed504435ad1dcf
Hartwig Anzt <hanzt@icl.utk.edu>
2018-04-17 10:31:32 -0400

    clean up jaccard weigths kernel

--- sparse/blas/zjaccard_weights.cu
+++ sparse/blas/zjaccard_weights.cu
@@ -118,7 +118,7 @@ magma_zjaccard_weights(
     int dimgrid1 = sqrt( magma_ceildiv( nnz, blocksize1 ) );
     int dimgrid2 = magma_ceildiv(nnz, blocksize1*dimgrid1);
     int dimgrid3 = 1;
-    printf("thread block: ( %d x %d  ) x [%d x %d]\n", blocksize1, blocksize2, dimgrid1, dimgrid2);
+    // printf("thread block: ( %d x %d  ) x [%d x %d]\n", blocksize1, blocksize2, dimgrid1, dimgrid2);
 
     // Runtime API
     // cudaFuncCachePreferShared: shared memory is 48 KB
@@ -141,6 +141,5 @@ magma_zjaccard_weights(
         A.dcol,
         A.dval );
 
-cleanup:
     return info;
 }
