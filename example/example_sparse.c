ed0fb56a67c8ca78dcccd055e8f28581984135b4
Hartwig Anzt <hanzt@icl.utk.edu>
2018-04-23 14:25:23 -0400

    clean up the example

--- example/example_sparse.c
+++ example/example_sparse.c
@@ -64,14 +64,15 @@ int main( int argc, char** argv )
     // Initialize the solver.
     magma_dsolverinfo_init( &opts.solver_par, &opts.precond_par, queue );
     
-    // Copy the system to the device (optional, only necessary if using the GPU)
+    // Copy the system to the device
     magma_dmtransfer( A, &dA, Magma_CPU, Magma_DEV, queue );
     magma_dmtransfer( b, &db, Magma_CPU, Magma_DEV, queue );
-    magma_dmtransfer( x, &dx, Magma_CPU, Magma_DEV, queue );
-    
+    // initialize an initial guess for the iteration vector
+    magma_dvinit( &dx, Magma_DEV, b.num_rows, b.num_cols, 0.0, queue );
+
     // Generate the preconditioner.
     magma_d_precondsetup( dA, db, &opts.solver_par, &opts.precond_par, queue );
-    
+
     // In case we only wanted to generate a preconditioner, we are done.
     // The preconditioner in the opts.precond_par structure - in this case an ILU.
     // The lower ILU(0) factor is in opts.precond_par.L and
@@ -79,25 +80,18 @@ int main( int argc, char** argv )
     
     // If we want to solve the problem, run:
     magma_d_solver( dA, db, &dx, &opts, queue );
-
+    
     // Then copy the solution back to the host...
-    magma_dmfree( &x, queue );
     magma_dmtransfer( dx, &x, Magma_DEV, Magma_CPU, queue );
-            for (magma_int_t i=0; i<x.num_rows*x.num_cols; i++) {
-            printf("x[%d] = %.2f\n", i, MAGMA_Z_REAL(x.val[i]));
-            //val[i] = v.val[i];
-        }
     // and back to the application code
-    magma_dvget( x, &m, &n, &sol, queue );
+    magma_dvget( x, &m, &n, sol, queue );
     
     // Free the allocated memory...
     magma_dmfree( &dx, queue );
     magma_dmfree( &db, queue ); 
     magma_dmfree( &dA, queue );
-    
     magma_dmfree( &b, queue );  // won't do anything as MAGMA does not own the data. 
     magma_dmfree( &A, queue );  // won't do anything as MAGMA does not own the data. 
-    
     // and finalize MAGMA.
     magma_queue_destroy( queue );
     magma_finalize();
