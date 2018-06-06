fb108edda017e4a07344167b794348bf77a4ff67
abdelfattah83 <devnull@localhost>
2018-06-06 13:22:11 +0200

    update signatures

--- include/magma_batched.h
+++ include/magma_batched.h
@@ -26,23 +26,38 @@ extern "C" {
 #endif
 
 
-void setup_pivinfo_batched( magma_int_t **pivinfo_array, magma_int_t **ipiv_array, 
-                              magma_int_t m, magma_int_t nb, 
-                              magma_int_t batchCount,  magma_queue_t queue);
+void 
+setup_pivinfo_batched( magma_int_t **pivinfo_array, magma_int_t **ipiv_array, magma_int_t ipiv_offset, 
+    magma_int_t m, magma_int_t nb, 
+    magma_int_t batchCount,  magma_queue_t queue);
 
 
-void adjust_ipiv_batched( magma_int_t **ipiv_array, 
-                       magma_int_t m, magma_int_t offset, 
-                       magma_int_t batchCount, magma_queue_t queue);
+void 
+adjust_ipiv_batched( magma_int_t **ipiv_array, magma_int_t ipiv_offset, 
+    magma_int_t m, magma_int_t offset, 
+    magma_int_t batchCount, magma_queue_t queue);
 
-void magma_idisplace_pointers(magma_int_t **output_array,
-               magma_int_t **input_array, magma_int_t lda,
-               magma_int_t row, magma_int_t column, 
-               magma_int_t batchCount, magma_queue_t queue);
+void 
+magma_idisplace_pointers(magma_int_t **output_array,
+    magma_int_t **input_array, magma_int_t lda,
+    magma_int_t row, magma_int_t column, 
+    magma_int_t batchCount, magma_queue_t queue);
 
-void stepinit_ipiv(magma_int_t **ipiv_array,
-                 magma_int_t pm,
-                 magma_int_t batchCount, magma_queue_t queue);
+void 
+stepinit_ipiv(magma_int_t **ipiv_array,
+    magma_int_t pm,
+    magma_int_t batchCount, magma_queue_t queue);
+
+void 
+setup_pivinfo(
+    magma_int_t *pivinfo, magma_int_t *ipiv, 
+    magma_int_t m, magma_int_t nb, 
+    magma_queue_t queue);
+
+void
+adjust_ipiv( magma_int_t *ipiv, 
+                 magma_int_t m, magma_int_t offset, 
+                 magma_queue_t queue);
 
 void magma_iset_pointer(
     magma_int_t **output_array,
