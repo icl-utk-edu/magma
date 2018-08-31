5d5630e2ee80a52f3f67b7077a119a9fa5a53202
abdelfattah83 <devnull@localhost>
2018-08-31 09:25:00 -0400

    tuning of GEMM on small sq sizes

--- control/get_ntcol.cpp
+++ control/get_ntcol.cpp
@@ -18,6 +18,27 @@
 const magma_int_t ntcol_1d_default[] = {NTCOL_1D_DEFAULT};
 
 // =============================================================================
+// GEMM
+// =============================================================================
+// Kepler (or older) 
+const magma_int_t sgemm_batched_ntcol_300[] = {64,32,32,32,10,8,10,4,3,5,2,2,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+const magma_int_t dgemm_batched_ntcol_300[] = {64,32,32,8,5,8,10,8,6,5,4,2,3,1,2,2,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+const magma_int_t cgemm_batched_ntcol_300[] = {64,32,32,64,5,8,10,10,8,5,4,2,3,1,2,2,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+const magma_int_t zgemm_batched_ntcol_300[] = {64,16,8,6,4,4,12,10,8,4,1,3,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+
+// Pascal (used also for maxwell) 
+const magma_int_t sgemm_batched_ntcol_600[] = {64,64,64,32,14,13,9,5,7,3,5,3,3,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+const magma_int_t dgemm_batched_ntcol_600[] = {64,64,32,16,16,8,10,1,8,8,2,2,3,5,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+const magma_int_t cgemm_batched_ntcol_600[] = {64,32,64,32,32,8,13,1,3,8,2,2,3,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+const magma_int_t zgemm_batched_ntcol_600[] = {64,14,14,14,32,8,16,2,4,2,4,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+
+// Volta  
+const magma_int_t sgemm_batched_ntcol_700[] = {64,64,32,8,10,8,10,8,8,10,4,2,3,5,2,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+const magma_int_t dgemm_batched_ntcol_700[] = {64,15,32,15,15,8,10,8,8,10,8,2,3,5,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+const magma_int_t cgemm_batched_ntcol_700[] = {64,64,16,32,8,8,10,8,8,5,2,2,3,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+const magma_int_t zgemm_batched_ntcol_700[] = {64,32,32,15,8,6,4,4,3,5,1,2,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+
+// =============================================================================
 // LU
 // =============================================================================
 // Kepler (or older) 
@@ -70,6 +91,72 @@ extern "C" {
 /// @addtogroup magma_tuning
 /// Tuning of the batched kernels that are invoked on extremely small sizes
 /// @{
+
+/***************************************************************************//**
+    @return the ntcol value for very small xgemm_batched ( m = n = k)
+*******************************************************************************/
+magma_int_t magma_get_zgemm_batched_ntcol(magma_int_t n)
+{
+    magma_int_t* ntcol_array; 
+
+    if(n < 0 || n > 32) return 1;
+    
+    magma_int_t arch = magma_getdevice_arch();
+    if      (arch <= 300) ntcol_array = (magma_int_t*)zgemm_batched_ntcol_300; 
+    else if (arch <= 600) ntcol_array = (magma_int_t*)zgemm_batched_ntcol_600;
+    else if (arch <= 700) ntcol_array = (magma_int_t*)zgemm_batched_ntcol_700;
+    else                  ntcol_array = (magma_int_t*)ntcol_1d_default; 
+    
+    return ntcol_array[m-1];
+}
+
+/// @see magma_get_zgemm_batched_ntcol
+magma_int_t magma_get_cgemm_batched_ntcol(magma_int_t n)
+{
+    magma_int_t* ntcol_array; 
+
+    if(n < 0 || n > 32) return 1;
+    
+    magma_int_t arch = magma_getdevice_arch();
+    if      (arch <= 300) ntcol_array = (magma_int_t*)cgemm_batched_ntcol_300; 
+    else if (arch <= 600) ntcol_array = (magma_int_t*)cgemm_batched_ntcol_600;
+    else if (arch <= 700) ntcol_array = (magma_int_t*)cgemm_batched_ntcol_700;
+    else                  ntcol_array = (magma_int_t*)ntcol_1d_default; 
+    
+    return ntcol_array[m-1];
+}
+
+/// @see magma_get_zgemm_batched_ntcol
+magma_int_t magma_get_dgemm_batched_ntcol(magma_int_t n)
+{
+    magma_int_t* ntcol_array; 
+
+    if(n < 0 || n > 32) return 1;
+    
+    magma_int_t arch = magma_getdevice_arch();
+    if      (arch <= 300) ntcol_array = (magma_int_t*)dgemm_batched_ntcol_300; 
+    else if (arch <= 600) ntcol_array = (magma_int_t*)dgemm_batched_ntcol_600;
+    else if (arch <= 700) ntcol_array = (magma_int_t*)dgemm_batched_ntcol_700;
+    else                  ntcol_array = (magma_int_t*)ntcol_1d_default; 
+    
+    return ntcol_array[m-1];
+}
+
+/// @see magma_get_zgemm_batched_ntcol
+magma_int_t magma_get_sgemm_batched_ntcol(magma_int_t n)
+{
+    magma_int_t* ntcol_array; 
+
+    if(n < 0 || n > 32) return 1;
+    
+    magma_int_t arch = magma_getdevice_arch();
+    if      (arch <= 300) ntcol_array = (magma_int_t*)sgemm_batched_ntcol_300; 
+    else if (arch <= 600) ntcol_array = (magma_int_t*)sgemm_batched_ntcol_600;
+    else if (arch <= 700) ntcol_array = (magma_int_t*)sgemm_batched_ntcol_700;
+    else                  ntcol_array = (magma_int_t*)ntcol_1d_default; 
+    
+    return ntcol_array[m-1];
+}
 
 /***************************************************************************//**
     @return the ntcol value for very small xgetrf_batched ( m = n )
