f8be1d33f9e4682dbe1a0c65e1a2fab37c4ba08d
Heike Jagode <jagode@icl.utk.edu>
2018-11-16 18:11:54 -0500

    Added PAPI SDEs for TFQMR and PTFQMR.

--- sparse/src/zptfqmr_merge.cpp
+++ sparse/src/zptfqmr_merge.cpp
@@ -11,6 +11,7 @@
 */
 
 #include "magmasparse_internal.h"
+#include "papi_sde_hook.h"
 
 #define RTOLERANCE     lapackf77_dlamch( "E" )
 #define ATOLERANCE     lapackf77_dlamch( "E" )
@@ -69,7 +70,10 @@ magma_zptfqmr_merge(
     solver_par->solver = Magma_TFQMRMERGE;
     solver_par->numiter = 0;
     solver_par->spmv_count = 0;
-    
+   
+    // Register PAPI SDE counters and recorders
+    magma_z_papi_sde_hook( solver_par );
+ 
     // local variables
     magmaDoubleComplex c_zero = MAGMA_Z_ZERO, c_one = MAGMA_Z_ONE;
     // solver variables
@@ -201,6 +205,12 @@ magma_zptfqmr_merge(
         }
 
         if ( res/nomb <= solver_par->rtol || res <= solver_par->atol ){
+            // PAPI SDE recorder of iterative residuals
+            if ( solver_par->sde_rcrd.magma_env_on != NULL ) {
+                papi_sde_record( solver_par->sde_rcrd.handle_iter_res,
+                                 sizeof(res), &res );
+            }
+
             info = MAGMA_SUCCESS;
             break;
         }
@@ -246,6 +256,12 @@ magma_zptfqmr_merge(
         queue );
         
         res = magma_dznrm2( dofs, r.dval, 1, queue );
+
+        // PAPI SDE recorder of iterative residuals
+        if ( solver_par->sde_rcrd.magma_env_on != NULL ) {
+            papi_sde_record( solver_par->sde_rcrd.handle_iter_res,
+                             sizeof(res), &res );
+        }
         
         if ( solver_par->verbose > 0 ) {
             tempo2 = magma_sync_wtime( queue );
--- sparse/src/ztfqmr_merge.cpp
+++ sparse/src/ztfqmr_merge.cpp
@@ -11,6 +11,7 @@
 */
 
 #include "magmasparse_internal.h"
+#include "papi_sde_hook.h"
 
 #define RTOLERANCE     lapackf77_dlamch( "E" )
 #define ATOLERANCE     lapackf77_dlamch( "E" )
@@ -64,7 +65,10 @@ magma_ztfqmr_merge(
     solver_par->solver = Magma_TFQMRMERGE;
     solver_par->numiter = 0;
     solver_par->spmv_count = 0;
-    
+   
+    // Register PAPI SDE counters and recorders
+    magma_z_papi_sde_hook( solver_par );
+ 
     // local variables
     magmaDoubleComplex c_zero = MAGMA_Z_ZERO, c_one = MAGMA_Z_ONE;
     // solver variables
@@ -171,7 +175,7 @@ magma_ztfqmr_merge(
         queue );
         
         res = magma_dznrm2( dofs, r.dval, 1, queue );
-        
+       
         if ( solver_par->verbose > 0 ) {
             tempo2 = magma_sync_wtime( queue );
             if ( (solver_par->numiter)%solver_par->verbose == 0 ) {
@@ -183,6 +187,12 @@ magma_ztfqmr_merge(
         }
 
         if ( res/nomb <= solver_par->rtol || res <= solver_par->atol ){
+            // PAPI SDE recorder of iterative residuals
+            if ( solver_par->sde_rcrd.magma_env_on != NULL ) {
+                papi_sde_record( solver_par->sde_rcrd.handle_iter_res,
+                                 sizeof(res), &res );
+            }
+ 
             info = MAGMA_SUCCESS;
             break;
         }
@@ -226,6 +236,12 @@ magma_ztfqmr_merge(
         
         res = magma_dznrm2( dofs, r.dval, 1, queue );
         
+        // PAPI SDE recorder of iterative residuals
+        if ( solver_par->sde_rcrd.magma_env_on != NULL ) {
+            papi_sde_record( solver_par->sde_rcrd.handle_iter_res,
+                             sizeof(res), &res );
+        }
+ 
         if ( solver_par->verbose > 0 ) {
             tempo2 = magma_sync_wtime( queue );
             if ( (solver_par->numiter)%solver_par->verbose == 0 ) {
