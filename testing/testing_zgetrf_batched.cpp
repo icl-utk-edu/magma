766ee597a05828d724919c6489708c1d7679efb9
abdelfattah83 <devnull@localhost>
2018-12-11 11:49:56 -0500

    use locinfo, not info

--- testing/testing_zgetrf_batched.cpp
+++ testing/testing_zgetrf_batched.cpp
@@ -200,7 +200,7 @@ int main( int argc, char** argv)
                     lapackf77_zgetrf(&M, &N, h_A + s * lda * N, &lda, ipiv + s * min_mn, &locinfo);
                     if (locinfo != 0) {
                         printf("lapackf77_zgetrf matrix %lld returned error %lld: %s.\n",
-                               (long long) s, (long long) info, magma_strerror( info ));
+                               (long long) s, (long long) locinfo, magma_strerror( locinfo ));
                     }
                 }
                 #if !defined (BATCHED_DISABLE_PARCPU) && defined(_OPENMP)
