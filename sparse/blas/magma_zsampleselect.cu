dd35e33c73268ed6103fe6ece8fb8fa4463783aa
abdelfattah83 <devnull@localhost>
2019-04-03 08:53:55 -0400

    fix a compilation problem

--- sparse/blas/magma_zsampleselect.cu
+++ sparse/blas/magma_zsampleselect.cu
@@ -15,11 +15,12 @@
 
 #define PRECISION_z
 
-#if (CUDA_ARCH >= 350)
 
 namespace magma_sampleselect {
 
-__global__ void compute_abs(const magmaDoubleComplex* __restrict__ in, double* __restrict__ out, int32_t size) {
+__global__ void compute_abs(const magmaDoubleComplex* __restrict__ in, double* __restrict__ out, int32_t size) 
+{
+#if (__CUDA_ARCH__ >= 350)
     auto idx = threadIdx.x + blockDim.x * blockIdx.x;
     if (idx >= size) {
         return;
@@ -27,6 +28,7 @@ __global__ void compute_abs(const magmaDoubleComplex* __restrict__ in, double* _
 
     auto v = in[idx];
     out[idx] = real(v) * real(v) + imag(v) * imag(v);
+#endif
 }
 
 } // namespace magma_sampleselect
@@ -112,8 +114,6 @@ cleanup:
     return info;
 }
 
-#endif
-
 /**
     Purpose
     -------
@@ -168,45 +168,43 @@ magma_zsampleselect_approx(
     magma_queue_t queue )
 {
     magma_int_t info = 0;
-    
     magma_int_t arch = magma_getdevice_arch();
-    
-#if (arch >= 350)
 
-    auto num_blocks = magma_ceildiv(total_size, block_size);
-    auto local_work = (total_size + num_threads - 1) / num_threads;
-    auto required_size = sizeof(double) * (total_size + searchtree_size)
+    if( arch >= 350 ) {
+        auto num_blocks = magma_ceildiv(total_size, block_size);
+        auto local_work = (total_size + num_threads - 1) / num_threads;
+        auto required_size = sizeof(double) * (total_size + searchtree_size)
                          + sizeof(int32_t) * (searchtree_width * (num_grouped_blocks + 1) + 1);
-    auto realloc_result = realloc_if_necessary(tmp_ptr, tmp_size, required_size);
+        auto realloc_result = realloc_if_necessary(tmp_ptr, tmp_size, required_size);
 
-    double* gputmp = (double*)*tmp_ptr;
-    double* gputree = gputmp + total_size;
-    uint32_t* gpubucketidx = (uint32_t*)(gputree + searchtree_size);
-    int32_t* gpurankout = (int32_t*)(gpubucketidx + 1);
-    int32_t* gpucounts = gpurankout + 1;
-    int32_t* gpulocalcounts = gpucounts + searchtree_width;
-    uint32_t bucketidx{};
+        double* gputmp = (double*)*tmp_ptr;
+        double* gputree = gputmp + total_size;
+        uint32_t* gpubucketidx = (uint32_t*)(gputree + searchtree_size);
+        int32_t* gpurankout = (int32_t*)(gpubucketidx + 1);
+        int32_t* gpucounts = gpurankout + 1;
+        int32_t* gpulocalcounts = gpucounts + searchtree_width;
+        uint32_t bucketidx{};
 
-    CHECK(realloc_result);
+        CHECK(realloc_result);
 
-    compute_abs<<<num_blocks, block_size, 0, queue->cuda_stream()>>>
-        (val, gputmp, total_size);
-    build_searchtree<<<1, sample_size, 0, queue->cuda_stream()>>>
-        (gputmp, gputree, total_size);
-    count_buckets<<<num_grouped_blocks, block_size, 0, queue->cuda_stream()>>>
-        (gputmp, gputree, gpulocalcounts, total_size, local_work);
-    reduce_counts<<<searchtree_width, num_grouped_blocks, 0, queue->cuda_stream()>>>
-        (gpulocalcounts, gpucounts, num_grouped_blocks);
-    sampleselect_findbucket<<<1, searchtree_width / 2, 0, queue->cuda_stream()>>>
-        (gpucounts, subset_size, gpubucketidx, gpurankout);
-    magma_getvector(1, sizeof(uint32_t), gpubucketidx, 1, &bucketidx, 1, queue);
-    magma_dgetvector(1, gputree + searchtree_width - 1 + bucketidx, 1, thrs, 1, queue);
-    *thrs = std::sqrt(*thrs);
-    
-#else
-    printf("error: this functionality needs CUDA architecture >= 3.5\n");
-    inf = ERR_NOT_SUPPORTED;
-#endif
+        compute_abs<<<num_blocks, block_size, 0, queue->cuda_stream()>>>
+            (val, gputmp, total_size);
+        build_searchtree<<<1, sample_size, 0, queue->cuda_stream()>>>
+            (gputmp, gputree, total_size);
+        count_buckets<<<num_grouped_blocks, block_size, 0, queue->cuda_stream()>>>
+            (gputmp, gputree, gpulocalcounts, total_size, local_work);
+        reduce_counts<<<searchtree_width, num_grouped_blocks, 0, queue->cuda_stream()>>>
+            (gpulocalcounts, gpucounts, num_grouped_blocks);
+        sampleselect_findbucket<<<1, searchtree_width / 2, 0, queue->cuda_stream()>>>
+            (gpucounts, subset_size, gpubucketidx, gpurankout);
+        magma_getvector(1, sizeof(uint32_t), gpubucketidx, 1, &bucketidx, 1, queue);
+        magma_dgetvector(1, gputree + searchtree_width - 1 + bucketidx, 1, thrs, 1, queue);
+        *thrs = std::sqrt(*thrs);
+    }
+    else {
+        printf("error: this functionality needs CUDA architecture >= 3.5\n");
+        info = MAGMA_ERR_NOT_SUPPORTED;
+    }
 
 cleanup:
     return info;
