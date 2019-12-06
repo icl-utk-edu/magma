c3efca38f637961764d601e6edb3916194a9c222
Cade Brown <cbrow216@vols.utk.edu>
2019-12-06 13:02:17 -0500

    Removed CUDA specific requirement for gemm templates

--- magmablas/gemm_template_device.cuh
+++ magmablas/gemm_template_device.cuh
@@ -43,7 +43,7 @@ void gemm_template_device_nn(
     T*       __restrict__ C, int LDC,
     T alpha, T beta )
 {
-#if (__CUDA_ARCH__ >= 200)
+#if (__CUDA_ARCH__ >= 200) || defined(HAVE_HIP)
     int idx = threadIdx.x;  // thread's m dimension
     int idy = threadIdx.y;  // thread's n dimension
 
@@ -229,7 +229,7 @@ void gemm_template_device_nn(
             }
         }
     }
-#endif /* (__CUDA_ARCH__ >= 200) */
+#endif /* (__CUDA_ARCH__ >= 200) || defined(HAVE_HIP) */
 }
 
 
@@ -245,7 +245,7 @@ void gemm_template_device_nt(
     T*       __restrict__ C, int LDC,
     T alpha, T beta )
 {
-#if (__CUDA_ARCH__ >= 200)
+#if (__CUDA_ARCH__ >= 200) || defined(HAVE_HIP)
     int idx = threadIdx.x;  // thread's m dimension
     int idy = threadIdx.y;  // thread's n dimension
 
@@ -435,7 +435,7 @@ void gemm_template_device_nt(
             }
         }
     }
-#endif /* (__CUDA_ARCH__ >= 200) */
+#endif /* (__CUDA_ARCH__ >= 200) || defined(HAVE_HIP) */
 }
 
 
@@ -451,7 +451,7 @@ void gemm_template_device_tn(
     T*       __restrict__ C, int LDC,
     T alpha, T beta )
 {
-#if (__CUDA_ARCH__ >= 200)
+#if (__CUDA_ARCH__ >= 200) || defined(HAVE_HIP)
     int idx = threadIdx.x;  // thread's m dimension
     int idy = threadIdx.y;  // thread's n dimension
 
@@ -644,7 +644,7 @@ void gemm_template_device_tn(
             }
         }
     }
-#endif /* (__CUDA_ARCH__ >= 200) */
+#endif /* (__CUDA_ARCH__ >= 200) || defined(HAVE_HIP) */
 }
 
 
@@ -660,7 +660,7 @@ void gemm_template_device_tt(
     T*       __restrict__ C, int LDC,
     T alpha, T beta )
 {
-#if (__CUDA_ARCH__ >= 200)
+#if (__CUDA_ARCH__ >= 200) || defined(HAVE_HIP)
     int idx = threadIdx.x;  // thread's m dimension
     int idy = threadIdx.y;  // thread's n dimension
 
@@ -854,7 +854,7 @@ void gemm_template_device_tt(
             }
         }
     }
-#endif /* (__CUDA_ARCH__ >= 200) */
+#endif /* (__CUDA_ARCH__ >= 200) || defined(HAVE_HIP) */
 }
 
 #endif //GEMM_TEMPLATE_DEVICE_CUH
