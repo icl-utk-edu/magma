f64c52aa334de75e1a3fff0e924d8b2c65b13e39
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2020-07-13 14:28:10 -0400

    support batches larger than 65k

--- magmablas/hemv_template_kernel_batched.cuh
+++ magmablas/hemv_template_kernel_batched.cuh
@@ -4,7 +4,7 @@
        Univ. of California, Berkeley
        Univ. of Colorado, Denver
        @date
-       
+
        @author Ahmad Abdelfattah
 */
 
@@ -19,51 +19,51 @@
 template <typename T, const int NB, const int TY>
 static __global__
 void hemv_diag_template_batched_kernel(
-        magma_uplo_t uplo, int n, 
-        T alpha, T** Aarray, int ldda, 
+        magma_uplo_t uplo, int n,
+        T alpha, T** Aarray, int ldda,
                  T** Xarray, int incx,
-        T beta,  T** Yarray, int incy,  
+        T beta,  T** Yarray, int incy,
         int offA, int offX, int offY)
 {
     int batchid = blockIdx.z;
-    
-    hemv_diag_device<T, NB, TY>( uplo, n, 
-                                 alpha, Aarray[batchid] + offA * ldda + offA, ldda, 
-                                        Xarray[batchid] + offX * incx, incx, 
+
+    hemv_diag_device<T, NB, TY>( uplo, n,
+                                 alpha, Aarray[batchid] + offA * ldda + offA, ldda,
+                                        Xarray[batchid] + offX * incx, incx,
                                  beta , Yarray[batchid] + offY * incy, incy );
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 template <typename T, const int NB, const int TY>
 static __global__
 void hemv_lower_template_batched_kernel(
-        int n, T alpha, 
-        T** Aarray, int ldda, 
+        int n, T alpha,
+        T** Aarray, int ldda,
         T** Xarray, int incx,
-        T** Yarray, int incy,  
+        T** Yarray, int incy,
         int offA, int offX, int offY)
 {
     int batchid = blockIdx.z;
-    
-    hemv_lower_device<T, NB, TY>( n, alpha, 
-                                  Aarray[batchid] + offA * ldda + offA, ldda, 
-                                  Xarray[batchid] + offX * incx, incx, 
+
+    hemv_lower_device<T, NB, TY>( n, alpha,
+                                  Aarray[batchid] + offA * ldda + offA, ldda,
+                                  Xarray[batchid] + offX * incx, incx,
                                   Yarray[batchid] + offY * incy, incy );
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 template <typename T, const int NB, const int TY>
 static __global__
 void hemv_upper_template_batched_kernel(
-        int n, T alpha, 
-        T** Aarray, int ldda, 
+        int n, T alpha,
+        T** Aarray, int ldda,
         T** Xarray, int incx,
-        T** Yarray, int incy,  
+        T** Yarray, int incy,
         int offA, int offX, int offY)
 {
     int batchid = blockIdx.z;
-    
-    hemv_upper_device<T, NB, TY>( n, alpha, 
-                                  Aarray[batchid] + offA * ldda + offA, ldda, 
-                                  Xarray[batchid] + offX * incx, incx, 
+
+    hemv_upper_device<T, NB, TY>( n, alpha,
+                                  Aarray[batchid] + offA * ldda + offA, ldda,
+                                  Xarray[batchid] + offX * incx, incx,
                                   Yarray[batchid] + offY * incy, incy );
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -73,66 +73,84 @@ void hemv_upper_template_batched_kernel(
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 template <typename T, const int NB, const int TY>
 void hemv_diag_template_batched(
-    magma_uplo_t uplo, magma_int_t n, 
+    magma_uplo_t uplo, magma_int_t n,
     T alpha, T** dA_array, magma_int_t ldda,
              T** dX_array, magma_int_t incx,
-    T beta,  T** dY_array, magma_int_t incy,  
-    magma_int_t offA, magma_int_t offX, magma_int_t offY, 
+    T beta,  T** dY_array, magma_int_t incy,
+    magma_int_t offA, magma_int_t offX, magma_int_t offY,
     magma_int_t batchCount, magma_queue_t queue)
 {
+    magma_int_t max_batchCount = 50000;
     dim3 threads(NB, TY, 1);
-    dim3 grid( magma_ceildiv( n, NB ), 1, batchCount );
-    hemv_diag_template_batched_kernel<T, NB, TY>
+
+    for(magma_int_t i = 0; i < batchCount; i+=max_batchCount) {
+        magma_int_t ibatch = min(max_batchCount, batchCount-i);
+        dim3 grid( magma_ceildiv( n, NB ), 1, ibatch );
+
+        hemv_diag_template_batched_kernel<T, NB, TY>
         <<< grid, threads, 0, queue->cuda_stream() >>>
-        ( uplo, n, 
-          alpha, dA_array, ldda, 
-                 dX_array, incx, 
-          beta,  dY_array, incy, 
-          offA, offX, offY);
+        ( uplo, n,
+          alpha, dA_array+i, ldda,
+                 dX_array+i, incx,
+          beta,  dY_array+i, incy,
+          offA, offX, offY );
+    }
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // lower
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 template <typename T, const int NB, const int TY>
 void hemv_lower_template_batched(
-    magma_int_t n, T alpha, 
+    magma_int_t n, T alpha,
     T** dA_array, magma_int_t ldda,
     T** dX_array, magma_int_t incx,
-    T** dY_array, magma_int_t incy,  
-    magma_int_t offA, magma_int_t offX, magma_int_t offY, 
+    T** dY_array, magma_int_t incy,
+    magma_int_t offA, magma_int_t offX, magma_int_t offY,
     magma_int_t batchCount, magma_queue_t queue)
 {
+    magma_int_t max_batchCount = 50000;
     dim3 threads(NB, TY, 1);
-    dim3 grid( magma_ceildiv( n, NB ), 1, batchCount );
-    hemv_lower_template_batched_kernel<T, NB, TY>
+
+    for(magma_int_t i = 0; i < batchCount; i+=max_batchCount) {
+        magma_int_t ibatch = min(max_batchCount, batchCount-i);
+        dim3 grid( magma_ceildiv( n, NB ), 1, ibatch );
+
+        hemv_lower_template_batched_kernel<T, NB, TY>
         <<< grid, threads, 0, queue->cuda_stream() >>>
-        ( n, alpha, 
-          dA_array, ldda, 
-          dX_array, incx, 
-          dY_array, incy, 
-          offA, offX, offY);
+        ( n, alpha,
+          dA_array+i, ldda,
+          dX_array+i, incx,
+          dY_array+i, incy,
+          offA, offX, offY );
+    }
 }
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // upper
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 template <typename T, const int NB, const int TY>
 void hemv_upper_template_batched(
-    magma_int_t n, T alpha, 
+    magma_int_t n, T alpha,
     T** dA_array, magma_int_t ldda,
     T** dX_array, magma_int_t incx,
-    T** dY_array, magma_int_t incy,  
-    magma_int_t offA, magma_int_t offX, magma_int_t offY, 
+    T** dY_array, magma_int_t incy,
+    magma_int_t offA, magma_int_t offX, magma_int_t offY,
     magma_int_t batchCount, magma_queue_t queue)
 {
+    magma_int_t max_batchCount = 50000;
     dim3 threads(NB, TY, 1);
-    dim3 grid( magma_ceildiv( n, NB ), 1, batchCount );
-    
-    hemv_upper_template_batched_kernel<T, NB, TY>
+
+    for(magma_int_t i = 0; i < batchCount; i+=max_batchCount) {
+        magma_int_t ibatch = min(max_batchCount, batchCount-i);
+        dim3 grid( magma_ceildiv( n, NB ), 1, ibatch );
+
+        hemv_upper_template_batched_kernel<T, NB, TY>
         <<< grid, threads, 0, queue->cuda_stream() >>>
-        ( n, alpha, 
-          dA_array, ldda, 
-          dX_array, incx, 
-          dY_array, incy, 
+        ( n, alpha,
+          dA_array+i, ldda,
+          dX_array+i, incx,
+          dY_array+i, incy,
           offA, offX, offY);
+    }
 }
 #endif //HEMV_TEMPLATE_KERNEL_BATCHED_CUH
