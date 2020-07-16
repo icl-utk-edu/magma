5d8c66ee6ceccc088d4d036dca3c8dd8605a18a1
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2020-07-16 13:46:31 -0400

    fix a bug in the batch hemv kernel that caused the kernel to hang for some sizes

--- magmablas/hemv_template_device.cuh
+++ magmablas/hemv_template_device.cuh
@@ -4,9 +4,9 @@
        Univ. of California, Berkeley
        Univ. of Colorado, Denver
        @date
-       
+
        @author Ahmad Abdelfattah
-       
+
        Originally based on an implementation by KBLAS (https://ecrc.kaust.edu.sa/Pages/Res-kblas.aspx)
 */
 
@@ -17,26 +17,26 @@
 /******************************************************************************/
 template <typename T, const int NB, const int TY>
 __device__ __inline__ void
-hemv_diag_device( magma_uplo_t uplo, int N, 
-                  T alpha, T *A, int ldda, 
-                           T *X, int incx, 
+hemv_diag_device( magma_uplo_t uplo, int N,
+                  T alpha, T *A, int ldda,
+                           T *X, int incx,
                   T beta , T *Y, int incy )
 {
     const int tx = threadIdx.x;
     const int ty = threadIdx.y;
     const int bx = blockIdx.x;
     const int n  = min(NB, N - bx * NB);
-    
+
     T res = make_FloatingPoint(0.0, 0.0);
     T ry  = make_FloatingPoint(0.0, 0.0);
-    
+
     __shared__ T sA[NB * NB];
     __shared__ T sX[NB];
-    
+
     A += bx * NB * (ldda + 1) + ty * ldda + tx;
     X += bx * NB * incx;
     Y += bx * NB * incy;
-    
+
     // init sA/sX to zeros
     #pragma unroll
     for(int i = 0; i < NB; i += TY){
@@ -46,13 +46,13 @@ hemv_diag_device( magma_uplo_t uplo, int N,
         sX[tx] = make_FloatingPoint(0.0, 0.0);
     }
     if(tx >= n) return;
-    
+
     // load x/y
     if(ty == 0 && tx < n){
-        sX[tx] = X[tx * incx]; 
+        sX[tx] = X[tx * incx];
         ry = Y[tx * incy] * beta;
     }
-    
+
     // read sA
     if(n < NB){
         int i;
@@ -66,11 +66,11 @@ hemv_diag_device( magma_uplo_t uplo, int N,
     }else{
         #pragma unroll
         for(int i = 0; i < NB; i+= TY)
-            sA[(i + ty) * NB + tx] = A[i * ldda];    
+            sA[(i + ty) * NB + tx] = A[i * ldda];
     }
     __syncthreads();
-    
-    // mirror 
+
+    // mirror
     if(uplo == MagmaLower){
         #pragma unroll
         for(int i = 0; i < NB; i+=TY){
@@ -87,23 +87,23 @@ hemv_diag_device( magma_uplo_t uplo, int N,
         }
     }
     __syncthreads();
-    
+
     // ignore imaginary part of diagonal elements
     if(ty == 0){
         sA[ tx * NB + tx ] = make_FloatingPoint( real(sA[ tx * NB + tx ]), 0.0 );
     }
     __syncthreads();
-    
-    // compute 
+
+    // compute
     #pragma unroll
     for(int i = 0; i < NB; i += TY){
         res += sA[ (i + ty) * NB + tx ] * sX[i + ty];
     }
-    
+
     __syncthreads();
     sA[ty * NB + tx] = res;
     __syncthreads();
-    
+
     if (ty == 0) {
         res = make_FloatingPoint( 0.0, 0.0 );
         #pragma unroll
@@ -118,52 +118,52 @@ hemv_diag_device( magma_uplo_t uplo, int N,
 
 /******************************************************************************/
 template <typename T, const int NB, const int TY>
-__device__ __inline__ void 
-hemv_lower_device( int N, T alpha, 
-                   T *A, int ldda, 
-                   T *X, int incx, 
+__device__ __inline__ void
+hemv_lower_device( int N, T alpha,
+                   T *A, int ldda,
+                   T *X, int incx,
                    T *Y, int incy )
 {
     const int tx = threadIdx.x;
     const int ty = threadIdx.y;
     const int bx = blockIdx.x;
     const int by = blockIdx.y;
-    T *X_, *Y_; 
+    T *X_, *Y_;
     T rA[EPT], rB[EPT];
     T rv[EPT];
     T rh = make_FloatingPoint(0.0, 0.0);
-    
+
     __shared__ T sA[NB * (NB+1)];
     __shared__ T sX[NB];
 
     const int gridx = magma_ceildiv(N, NB);    // do not use gridDim.x (doesn't work for vbatched)
     const int nfull = (gridx-bx-2); // exclude the diagonal block and the last full/partial block
-    const int start = by * (nfull/gridDim.y) + min(by, nfull%gridDim.y); 
+    const int start = by * (nfull/gridDim.y) + min(by, nfull%gridDim.y);
     const int count = nfull/gridDim.y + ( by < (nfull%gridDim.y) );
     if( (bx == gridx-1) || (by < gridDim.y-1 && count == 0))return;
-    
+
     A += bx * NB * (ldda + 1) + start * NB + ty * ldda + tx;
     X += bx * NB * incx;
     X_ = X;
-    X += start * NB * incx; 
+    X += start * NB * incx;
     Y += bx * NB * incy;
     Y_ = Y;
-    Y_+= start * NB * incy; 
-    
+    Y_+= start * NB * incy;
+
     if(ty == 0){
         sX[tx] = X_[tx * incx];
     }
-    __syncthreads(); 
-    
+    __syncthreads();
+
     #pragma unroll
     for(int i = 0; i < EPT; i++){
         rv[i] = make_FloatingPoint(0.0, 0.0);
     }
-    
+
     A  += NB;
     X  += NB * incx;
     Y_ += NB * incy;
-    
+
     if(count > 0){
         #pragma unroll
         for(int k = 0; k < EPT; k++){
@@ -176,7 +176,7 @@ hemv_lower_device( int N, T alpha,
         for(int k = 0; k < EPT; k++){
             rA[k] = rB[k];
         }
-        
+
         A  += NB;
         if(i < count-1){
             #pragma unroll
@@ -184,7 +184,7 @@ hemv_lower_device( int N, T alpha,
                 rB[k] = A[k * TY * ldda];
             }
         }
-        
+
         rh = make_FloatingPoint(0.0, 0.0);
         #pragma unroll
         for(int k = 0; k < EPT; k++){
@@ -197,59 +197,61 @@ hemv_lower_device( int N, T alpha,
         sA[ty * (NB+1) + tx] = rh;
         __syncthreads();
         if(ty == 0)
-        { 
+        {
             rh = make_FloatingPoint(0.0, 0.0);
             #pragma unroll
             for (int k = 0; k < TY; k++) {
                 rh += sA[k * (NB+1) + tx];
             }
             rh *= alpha;
-            
+
             magmablas_atomic_add(&Y_[incy * tx], rh);
         }
         X  += NB * incx;
         Y_ += NB * incy;
     }
-    
+
     // last irregular block
     const int n = N - (bx+nfull+1)*NB;    // size of remaining full/partial block
-    if(by == gridDim.y-1 && tx < n){
-        #pragma unroll
-        for(int k = 0; k < EPT; k++){
-            rA[k] = A[k * TY * ldda];
-        }
+    if(by == gridDim.y-1){
+        if(tx < n) {
+            #pragma unroll
+            for(int k = 0; k < EPT; k++){
+                rA[k] = A[k * TY * ldda];
+            }
 
-        rh = make_FloatingPoint(0.0, 0.0);
-        #pragma unroll
-        for(int k = 0; k < EPT; k++){
-            rh += rA[k] * sX[k * TY + ty];
-            rv[k] += conj( rA[k] ) * X[tx * incx];
+            rh = make_FloatingPoint(0.0, 0.0);
+            #pragma unroll
+            for(int k = 0; k < EPT; k++){
+                rh += rA[k] * sX[k * TY + ty];
+                rv[k] += conj( rA[k] ) * X[tx * incx];
+            }
         }
-
         // Horizontal block should be stored in global memory
         __syncthreads();
-        sA[ty * (NB+1) + tx] = rh;
+        if(tx < n) {
+            sA[ty * (NB+1) + tx] = rh;
+        }
         __syncthreads();
-        if(ty == 0)
-        { 
+        if(ty == 0 && tx < n) {
             rh = make_FloatingPoint(0.0, 0.0);
             #pragma unroll
             for (int k = 0; k < TY; k++) {
                 rh += sA[k * (NB+1) + tx];
             }
             rh *= alpha;
-            
+
             magmablas_atomic_add(&Y_[incy * tx], rh);
         }
     }
-    
+
     __syncthreads();
     #pragma unroll
     for(int k = 0; k < EPT; k++){
         sA[(k * TY + ty) * (NB+1) + tx] = rv[k];
     }
     __syncthreads();
-    
+
     if(ty == 0){
         rv[0] = make_FloatingPoint(0.0, 0.0);
         #pragma unroll
@@ -263,59 +265,59 @@ hemv_lower_device( int N, T alpha,
 
 /******************************************************************************/
 template <typename T, const int NB, const int TY>
-__device__ __inline__ void 
-hemv_upper_device( int N, T alpha, 
-                   T *A, int ldda, 
-                   T *X, int incx, 
+__device__ __inline__ void
+hemv_upper_device( int N, T alpha,
+                   T *A, int ldda,
+                   T *X, int incx,
                    T *Y, int incy )
 {
     const int tx = threadIdx.x;
     const int ty = threadIdx.y;
     const int bx = blockIdx.x;
     const int by = blockIdx.y;
-    T *X_, *Y_; 
+    T *X_, *Y_;
     T rA[EPT], rB[EPT];
     T rv[EPT];
-    int addr[EPT]; 
+    int addr[EPT];
     T rh = make_FloatingPoint(0.0, 0.0);
 
     __shared__ T sA[NB * (NB+1)];
     __shared__ T sX[NB];
 
     const int gridx = magma_ceildiv(N, NB);    // do not use gridDim.x (doesn't work for vbatched)
-    const int nr = N - (gridx-1) * NB; 
+    const int nr = N - (gridx-1) * NB;
     const int nblocks = bx;
-    const int start = by * (nblocks/gridDim.y) + min(by, nblocks%gridDim.y); 
+    const int start = by * (nblocks/gridDim.y) + min(by, nblocks%gridDim.y);
     const int count = nblocks/gridDim.y + ( by < (nblocks%gridDim.y) );
     if( bx == 0 || count == 0)return;
-    
+
     if(bx == gridx-1 && nr < NB)
         A += bx * NB * ldda + start * NB; // + ty * ldda + tx;
-    else 
+    else
         A += bx * NB * ldda + start * NB + ty * ldda + tx;
-    
+
     X_ = X + bx * NB * incx;
-    X += start * NB * incx; 
-    Y_ = Y + start * NB * incy; 
-    Y += bx * NB * incy; 
-    
+    X += start * NB * incx;
+    Y_ = Y + start * NB * incy;
+    Y += bx * NB * incy;
+
     // init
     if(ty == 0) sX[tx] = make_FloatingPoint(0.0, 0.0);
     if(bx == gridx-1 && nr < NB){
         #pragma unroll
         for(int i = 0; i < EPT; i++){
             rv[i] = make_FloatingPoint(0.0, 0.0);
-            addr[i] = min(i*TY + ty, nr-1) * ldda + tx; 
+            addr[i] = min(i*TY + ty, nr-1) * ldda + tx;
         }
     }
     else{
         #pragma unroll
         for(int i = 0; i < EPT; i++){
             rv[i] = make_FloatingPoint(0.0, 0.0);
-            addr[i] = i * TY * ldda; 
+            addr[i] = i * TY * ldda;
         }
     }
-    
+
     if(bx == gridx-1 && nr < NB){
         if(ty == 0 && tx < nr)
             sX[tx] = X_[tx * incx];
@@ -324,12 +326,12 @@ hemv_upper_device( int N, T alpha,
         if(ty == 0)
             sX[tx] = X_[tx * incx];
     }
-    __syncthreads(); 
-    
+    __syncthreads();
+
     #pragma unroll
     for(int k = 0; k < EPT; k++)
         rB[k] = A[ addr[k] ];
-    
+
     #pragma unroll
     for(int i = 0; i < count; i++){
         #pragma unroll
@@ -342,7 +344,7 @@ hemv_upper_device( int N, T alpha,
             for(int k = 0; k < EPT; k++)
                 rB[k] = A[ addr[k] ];
         }
-        
+
         rh = make_FloatingPoint(0.0, 0.0);
         #pragma unroll
         for(int k = 0; k < EPT; k++){
@@ -355,27 +357,27 @@ hemv_upper_device( int N, T alpha,
         sA[ty * (NB+1) + tx] = rh;
         __syncthreads();
         if(ty == 0)
-        { 
+        {
             rh = make_FloatingPoint(0.0, 0.0);
             #pragma unroll
             for (int k = 0; k < TY; k++)
                 rh += sA[k * (NB+1) + tx];
-               
+
             rh *= alpha;
-            
+
             magmablas_atomic_add(&Y_[incy * tx], rh);
         }
         X  += NB * incx;
         Y_ += NB * incy;
     }
-    
+
     __syncthreads();
     #pragma unroll
     for(int k = 0; k < EPT; k++){
         sA[(k * TY + ty) * (NB+1) + tx] = rv[k];
     }
     __syncthreads();
-    
+
     if(ty == 0){
         rv[0] = make_FloatingPoint(0.0, 0.0);
         #pragma unroll
