127166eef0745b2ae29301f781d455640ca82a8b
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2020-07-16 10:46:18 -0400

    an overhaul for this tester

--- testing/testing_zlacpy_batched.cpp
+++ testing/testing_zlacpy_batched.cpp
@@ -33,116 +33,131 @@ int main( int argc, char** argv)
     real_Double_t    gbytes, gpu_perf, gpu_time, cpu_perf, cpu_time;
     double           error, work[1];
     magmaDoubleComplex  c_neg_one = MAGMA_Z_NEG_ONE;
-    magmaDoubleComplex *h_A, *h_B;
+    magmaDoubleComplex *h_A, *h_B, *h_R;
     magmaDoubleComplex_ptr d_A, d_B;
-    magmaDoubleComplex **hAarray, **hBarray, **dAarray, **dBarray;
-    magma_int_t M, N, mb, nb, size, lda, ldda, mstride, nstride, ntile;
+    magmaDoubleComplex **hA_array, **hB_array, **dA_array, **dB_array;
+    magma_int_t M, N, mb, nb, sizeA, sizeB, lda, ldda, ldb, lddb;
     magma_int_t ione     = 1;
     magma_int_t ISEED[4] = {0,0,0,1};
     int status = 0;
-    
+
     magma_opts opts( MagmaOptsBatched );
     opts.parse_opts( argc, argv );
+    magma_int_t batchCount = opts.batchCount;
 
-    mb = (opts.nb == 0 ? 32 : opts.nb);
-    nb = (opts.nb == 0 ? 64 : opts.nb);
-    mstride = 2*mb;
-    nstride = 3*nb;
-    
-    printf("%% mb=%lld, nb=%lld, mstride=%lld, nstride=%lld\n", (long long) mb, (long long) nb, (long long) mstride, (long long) nstride );
-    printf("%%   M     N ntile    CPU Gflop/s (ms)    GPU Gflop/s (ms)   check\n");
-    printf("%%================================================================\n");
-    for( int itest = 0; itest < opts.ntest; ++itest ) {
-        for( int iter = 0; iter < opts.niter; ++iter ) {
+    magma_uplo_t uplo[] = { MagmaLower, MagmaUpper, MagmaFull };
+    printf("%% BatchCount   uplo    M     N   CPU GByte/s (ms)    GPU GByte/s (ms)    check\n");
+    printf("%%=============================================================================\n");
+    for( int iuplo = 0; iuplo < 3; ++iuplo ) {
+        for( int itest = 0; itest < opts.ntest; ++itest ) {
+            for( int iter = 0; iter < opts.niter; ++iter ) {
             M = opts.msize[itest];
             N = opts.nsize[itest];
-            lda    = M;
-            ldda   = magma_roundup( M, opts.align );  // multiple of 32 by default
-            size   = lda*N;
-            
-            if ( N < nb || M < nb ) {
-                ntile = 0;
-            } else {
-                ntile = min( (M - nb)/mstride + 1,
-                             (N - nb)/nstride + 1 );
+            lda   = M;
+            ldb   = lda;
+            ldda  = magma_roundup( M, opts.align );  // multiple of 32 by default
+            lddb  = ldda;
+            sizeA = lda*N;
+            sizeB = ldb*N;
+
+            if ( uplo[iuplo] == MagmaLower ) {
+                // load & save lower trapezoid (with diagonal)
+                if ( M > N ) {
+                    gbytes = batchCount * 2. * sizeof(magmaDoubleComplex) * (1.*M*N - 0.5*N*(N-1)) / 1e9;
+                } else {
+                    gbytes = batchCount * 2. * sizeof(magmaDoubleComplex) * 0.5*M*(M+1) / 1e9;
+                }
             }
-            gbytes = 2.*mb*nb*ntile / 1e9;
-            
-            TESTING_CHECK( magma_zmalloc_cpu( &h_A, lda *N ));
-            TESTING_CHECK( magma_zmalloc_cpu( &h_B, lda *N ));
-            TESTING_CHECK( magma_zmalloc( &d_A, ldda*N ));
-            TESTING_CHECK( magma_zmalloc( &d_B, ldda*N ));
-            
-            TESTING_CHECK( magma_malloc_cpu( (void**) &hAarray, ntile * sizeof(magmaDoubleComplex*) ));
-            TESTING_CHECK( magma_malloc_cpu( (void**) &hBarray, ntile * sizeof(magmaDoubleComplex*) ));
-            TESTING_CHECK( magma_malloc( (void**) &dAarray, ntile * sizeof(magmaDoubleComplex*) ));
-            TESTING_CHECK( magma_malloc( (void**) &dBarray, ntile * sizeof(magmaDoubleComplex*) ));
-            
-            lapackf77_zlarnv( &ione, ISEED, &size, h_A );
-            lapackf77_zlarnv( &ione, ISEED, &size, h_B );
+            else if ( uplo[iuplo] == MagmaUpper ) {
+                // load & save upper trapezoid (with diagonal)
+                if ( N > M ) {
+                    gbytes = batchCount * 2. * sizeof(magmaDoubleComplex) * (1.*M*N - 0.5*M*(M-1)) / 1e9;
+                } else {
+                    gbytes = batchCount * 2. * sizeof(magmaDoubleComplex) * 0.5*N*(N+1) / 1e9;
+                }
+            }
+            else {
+                // load & save entire matrix
+                gbytes = batchCount * 2. * sizeof(magmaDoubleComplex) * 1.*M*N / 1e9;
+            }
+
+            TESTING_CHECK( magma_zmalloc_cpu( &h_A, batchCount * lda * N ));
+            TESTING_CHECK( magma_zmalloc_cpu( &h_B, batchCount * ldb * N ));
+            TESTING_CHECK( magma_zmalloc_cpu( &h_R, batchCount * ldb * N ));
+            TESTING_CHECK( magma_zmalloc( &d_A, batchCount * ldda*N ));
+            TESTING_CHECK( magma_zmalloc( &d_B, batchCount * ldda*N ));
+
+            TESTING_CHECK( magma_malloc( (void**) &dA_array, batchCount*sizeof(magmaDoubleComplex*) ));
+            TESTING_CHECK( magma_malloc( (void**) &dB_array, batchCount*sizeof(magmaDoubleComplex*) ));
+            TESTING_CHECK( magma_malloc_cpu( (void**) &hA_array, batchCount * sizeof(magmaDoubleComplex*) ));
+            TESTING_CHECK( magma_malloc_cpu( (void**) &hB_array, batchCount * sizeof(magmaDoubleComplex*) ));
+
+
+            lapackf77_zlarnv( &ione, ISEED, &sizeA, h_A );
+            lapackf77_zlarnv( &ione, ISEED, &sizeA, h_B );
 
             /* ====================================================================
                Performs operation using MAGMA
                =================================================================== */
-            magma_zsetmatrix( M, N, h_A, lda, d_A, ldda, opts.queue );
-            magma_zsetmatrix( M, N, h_B, lda, d_B, ldda, opts.queue );
-            
+            magma_zsetmatrix( M, N*batchCount, h_A, lda, d_A, ldda, opts.queue );
+            magma_zsetmatrix( M, N*batchCount, h_B, lda, d_B, ldda, opts.queue );
+
             // setup pointers
-            for( magma_int_t tile = 0; tile < ntile; ++tile ) {
-                magma_int_t offset = tile*mstride + tile*nstride*ldda;
-                hAarray[tile] = &d_A[offset];
-                hBarray[tile] = &d_B[offset];
-            }
-            magma_setvector( ntile, sizeof(magmaDoubleComplex*), hAarray, 1, dAarray, 1, opts.queue );
-            magma_setvector( ntile, sizeof(magmaDoubleComplex*), hBarray, 1, dBarray, 1, opts.queue );
-            
+            magma_zset_pointer( dA_array, d_A, ldda, 0, 0, ldda*N, batchCount, opts.queue );
+            magma_zset_pointer( dB_array, d_B, lddb, 0, 0, lddb*N, batchCount, opts.queue );
+
             gpu_time = magma_sync_wtime( opts.queue );
-            magmablas_zlacpy_batched( MagmaFull, mb, nb, dAarray, ldda, dBarray, ldda, ntile, opts.queue );
+            magmablas_zlacpy_batched( uplo[iuplo], M, N, dA_array, ldda, dB_array, ldda, batchCount, opts.queue );
             gpu_time = magma_sync_wtime( opts.queue ) - gpu_time;
             gpu_perf = gbytes / gpu_time;
-            
+
             /* =====================================================================
                Performs operation using LAPACK
                =================================================================== */
-            cpu_time = magma_wtime();
-            for( magma_int_t tile = 0; tile < ntile; ++tile ) {
-                magma_int_t offset = tile*mstride + tile*nstride*lda;
-                lapackf77_zlacpy( MagmaFullStr, &mb, &nb,
-                                  &h_A[offset], &lda,
-                                  &h_B[offset], &lda );
+            for(int s = 0; s < batchCount; s++) {
+                hA_array[s] = h_A + s * lda * N;
+                hB_array[s] = h_B + s * ldb * N;
             }
+
+            cpu_time = magma_wtime();
+            blas_zlacpy_batched(uplo[iuplo], m, n, hA_array, lda, hB_array, ldb, batchCount );
             cpu_time = magma_wtime() - cpu_time;
             cpu_perf = gbytes / cpu_time;
-            
+
             /* =====================================================================
                Check the result
                =================================================================== */
-            magma_zgetmatrix( M, N, d_B, ldda, h_A, lda, opts.queue );
-            
-            blasf77_zaxpy(&size, &c_neg_one, h_A, &ione, h_B, &ione);
-            error = lapackf77_zlange("f", &M, &N, h_B, &lda, work);
+            magma_zgetmatrix( M, N*batchCount, d_B, ldda, h_R, ldb, opts.queue );
+            blasf77_zaxpy(&sizeB, &c_neg_one, h_B, &ione, h_R, &ione);
+
+            magma_int_t NN = batchCount*N;
+            error = lapackf77_zlange("f", &M, &NN, h_R, &ldb, work);
             bool okay = (error == 0);
             status += ! okay;
 
-            printf("%5lld %5lld %5lld   %7.2f (%7.2f)   %7.2f (%7.2f)   %s\n",
-                   (long long) M, (long long) N, (long long) ntile,
+            printf("  %10lld %5s %5lld %5lld   %7.2f (%7.2f)   %7.2f (%7.2f)   %s\n",
+                   (long long) batchCount, lapack_uplo_const(uplo[iuplo]),
+                   (long long) M, (long long) N,
                    cpu_perf, cpu_time*1000., gpu_perf, gpu_time*1000.,
                    (okay ? "ok" : "failed") );
-            
+
             magma_free_cpu( h_A );
             magma_free_cpu( h_B );
+            magma_free_cpu( h_R );
             magma_free( d_A );
             magma_free( d_B );
-            
-            magma_free_cpu( hAarray );
-            magma_free_cpu( hBarray );
-            magma_free( dAarray );
-            magma_free( dBarray );
+
+            magma_free_cpu( hA_array );
+            magma_free_cpu( hB_array );
+            magma_free( dA_array );
+            magma_free( dB_array );
             fflush( stdout );
         }
-        if ( opts.niter > 1 ) {
-            printf( "\n" );
+            if ( opts.niter > 1 ) {
+                printf( "\n" );
+            }
         }
+        printf("\n");
     }
 
     opts.cleanup();
