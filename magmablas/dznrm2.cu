14a88dfceff4c757fb3e7276876254e27586b54c
Stan Tomov <tomov@eecs.utk.edu>
2020-08-02 23:55:52 -0400

    a bug fix that fixes input with linearly dependent columns. There is still todo item related to need to recompute norms

--- magmablas/dznrm2.cu
+++ magmablas/dznrm2.cu
@@ -220,11 +220,16 @@ magma_dznrm2_row_check_adjust_kernel(
         double temp2 = xnorm[tx] / xnorm2[tx];
         temp2 = temp * (temp2 * temp2);
         
+        // todo: check this accuracy procedure; currently is not working for
+        //       constant matrix so it is disabled for now
+        /*
         if (temp2 <= tol) {
             lsticc[tx+1] = 1;
         } else {
             xnorm[tx] *= sqrt(temp);
         }
+        */
+        xnorm[tx] *= sqrt(temp);
     }
     if (tx == 0)
         lsticc[0] = 0;
