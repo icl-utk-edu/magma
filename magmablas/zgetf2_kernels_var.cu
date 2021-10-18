f5456358ef1509a976a05a1cc69b13a6562a39b1
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2021-10-18 16:25:33 -0400

    place holder for getf2_vbatched

---
+++ magmablas/zgetf2_kernels_var.cu
@@ -0,0 +1,21 @@
+/*
+    -- MAGMA (version 2.0) --
+       Univ. of Tennessee, Knoxville
+       Univ. of California, Berkeley
+       Univ. of Colorado, Denver
+       @date
+
+       @author Azzam Haidar
+       @author Tingxing Dong
+       @author Ahmad Abdelfattah
+
+       @precisions normal z -> s d c
+*/
+
+#include "magma_internal.h"
+#include "batched_kernel_param.h"
+#include "magma_templates.h"
+#include "shuffle.cuh"
+#include "zgetf2_devicefunc.h"
+
+#define PRECISION_z
