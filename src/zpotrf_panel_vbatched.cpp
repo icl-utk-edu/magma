5153945d7df97ca737618ea01bc50bbada03ddaa
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2021-11-22 16:11:49 -0500

    renaming existing vbatched trsm routine in preparation for a new one

--- src/zpotrf_panel_vbatched.cpp
+++ src/zpotrf_panel_vbatched.cpp
@@ -4,7 +4,7 @@
        Univ. of California, Berkeley
        Univ. of Colorado, Denver
        @date
-       
+
        @author Azzam Haidar
        @author Tingxing Dong
 
@@ -17,14 +17,14 @@
 ////////////////////////////////////////////////////////////////////////////////////////
 extern "C" magma_int_t
 magma_zpotrf_panel_vbatched(
-    magma_uplo_t uplo, magma_int_t* n, magma_int_t max_n, 
-    magma_int_t *ibvec, magma_int_t nb,  
+    magma_uplo_t uplo, magma_int_t* n, magma_int_t max_n,
+    magma_int_t *ibvec, magma_int_t nb,
     magmaDoubleComplex** dA_array,    magma_int_t* ldda,
     magmaDoubleComplex** dX_array,    magma_int_t* dX_length,
     magmaDoubleComplex** dinvA_array, magma_int_t* dinvA_length,
-    magmaDoubleComplex** dW0_displ, magmaDoubleComplex** dW1_displ, 
+    magmaDoubleComplex** dW0_displ, magmaDoubleComplex** dW1_displ,
     magmaDoubleComplex** dW2_displ, magmaDoubleComplex** dW3_displ,
-    magmaDoubleComplex** dW4_displ, 
+    magmaDoubleComplex** dW4_displ,
     magma_int_t *info_array, magma_int_t gbstep,
     magma_int_t batchCount, magma_queue_t queue)
 {
@@ -43,17 +43,17 @@ magma_zpotrf_panel_vbatched(
         // n-ib
         magma_ivec_add( batchCount, 1, n, -1, ibvec, n_minus_ib, queue);
         magma_zdisplace_pointers_var_cc(dW0_displ, dA_array, ldda, nb, 0, batchCount, queue);
-        magmablas_ztrsm_work_vbatched( MagmaRight, MagmaLower, MagmaConjTrans, MagmaNonUnit, 1,  
-                                      n_minus_ib, ibvec, 
-                                      MAGMA_Z_ONE, 
-                                      dA_array,  ldda, 
-                                      dW0_displ, ldda, 
-                                      dX_array,  n_minus_ib, 
-                                      dinvA_array, dinvA_length, 
-                                      dW1_displ, dW2_displ, 
-                                      dW3_displ, dW4_displ, 
-                                      0, batchCount, 
-                                      max_n-nb, nb, queue );
+        magmablas_ztrsm_inv_work_vbatched( MagmaRight, MagmaLower, MagmaConjTrans, MagmaNonUnit, 1,
+                                           n_minus_ib, ibvec,
+                                           MAGMA_Z_ONE,
+                                           dA_array,  ldda,
+                                           dW0_displ, ldda,
+                                           dX_array,  n_minus_ib,
+                                           dinvA_array, dinvA_length,
+                                           dW1_displ, dW2_displ,
+                                           dW3_displ, dW4_displ,
+                                           0, batchCount,
+                                           max_n-nb, nb, queue );
     }
     magma_free( n_minus_ib );
     return arginfo;
