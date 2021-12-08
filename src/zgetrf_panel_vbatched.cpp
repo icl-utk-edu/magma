9fbda14da356d02d0394fdaee0b406dafdd0773d
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2021-12-08 10:56:17 -0500

    minor chanhe in the API

--- src/zgetrf_panel_vbatched.cpp
+++ src/zgetrf_panel_vbatched.cpp
@@ -14,9 +14,9 @@
 
 magma_int_t
 magma_zgetrf_recpanel_vbatched(
-    magma_int_t* m, magma_int_t* n,
-    magma_int_t max_m, magma_int_t max_n,
-    magma_int_t max_minmn, magma_int_t max_mxn, magma_int_t min_recpnb,
+    magma_int_t* m, magma_int_t* n, magma_int_t* minmn,
+    magma_int_t max_m, magma_int_t max_n, magma_int_t max_minmn,
+    magma_int_t max_mxn, magma_int_t min_recpnb,
     magmaDoubleComplex** dA_array, magma_int_t Ai, magma_int_t Aj, magma_int_t* ldda,
     magma_int_t** dipiv_array, magma_int_t dipiv_i, magma_int_t** dpivinfo_array,
     magma_int_t *info_array, magma_int_t gbstep,
@@ -26,18 +26,12 @@ magma_zgetrf_recpanel_vbatched(
 #define ipiv_array(i)   dipiv_array, i
 
 
-    magma_int_t *minmn;
-    magma_imalloc(&minmn, batchCount);
-    magma_ivec_min_vv( batchCount, m, n, minmn, queue);
-
     magma_zgetf2_vbatched(
         m, n, minmn,
         max_m, max_n, max_minmn,
         dA_array, Ai, Aj, ldda,
         dipiv_array, info_array,
         0, batchCount, queue);
-
-    magma_free( minmn );
 
     return 0;
 
--- testing/testing_zgetrf_vbatched.cpp
+++ testing/testing_zgetrf_vbatched.cpp
@@ -205,8 +205,12 @@ int main( int argc, char** argv)
 
             magma_time = magma_sync_wtime( opts.queue );
             if(opts.version == 1) {
+                magma_int_t *minmn;
+                magma_imalloc(&minmn, batchCount);
+                magma_ivec_min_vv( batchCount, m, n, minmn, queue);
                 magma_memset(dinfo, 0, batchCount*sizeof(magma_int_t));
                 info = magma_zgetrf_recpanel_vbatched(d_M, d_N, max_M, max_N, max_minmn, 0, 0, dA_array, 0, 0, d_ldda, dipiv_array, 0, NULL, dinfo, 0, batchCount,  opts.queue);
+                magmafree(minmn);
             }
             else if(opts.version == 2) {
                 for(int s = 0; s < batchCount; s++) {
