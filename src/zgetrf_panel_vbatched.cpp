fbfff26b2a0e9d19b85a591472f347ec6e434ce5
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2021-12-09 11:23:32 -0500

    add code for debugging

--- src/zgetrf_panel_vbatched.cpp
+++ src/zgetrf_panel_vbatched.cpp
@@ -123,7 +123,7 @@ magma_zgetrf_recpanel_vbatched(
             dipiv_array,
             batchCount, queue);
         #ifdef DBG
-        printf("swap left - (k1, k2) = (%d, %d)\n");
+        printf("swap left - (k1, k2) = (%d, %d)\n", Ai+max_n1, Ai+max_n);
         magma_zprint_gpu(h_M[0], h_N[0], hA_array[0], h_ldda[0], queue);
         #endif
 
