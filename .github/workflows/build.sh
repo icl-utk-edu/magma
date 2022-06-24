8eb054270abb94fa01d43f2fc6f10c53f74b26bc
G-Ragghianti <ragghianti@icl.utk.edu>
2022-06-24 18:56:19 -0400

    Testing CI

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -9,24 +9,30 @@ compiler=$4
 source ~/spack_setup
 GCC=gcc@8.5.0
 sload cmake %$GCC
-sload openblas@0.3.10 %$GCC
-sload $cuda %$GCC
+sload openblas %$GCC
+sload cuda@11.4.3 %$GCC
 sload intel-oneapi-mkl %$GCC
 sload intel-oneapi-compilers %$GCC
 
 export CUDADIR=$CUDA_HOME
 export GPU_TARGET=Volta
-export OPENBLASDIR=`slocation openblas@0.3.10 %$GCC`
+export OPENBLASDIR=`slocation openblas %$GCC`
 
-(
-cd make.inc-examples
-cp make.inc.mkl-gcc-ilp64 make.inc.mkl-ilp64-gcc
-cp make.inc.mkl-icc-ilp64 make.inc.mkl-ilp64-icc
-cp make.inc.openblas make.inc.openblas-gcc
-cp make.inc.openblas make.inc.openblas-icc
-)
+if [ "$maker" = "make" ]; then
+   cd make.inc-examples
+   cp make.inc.mkl-gcc-ilp64 make.inc.mkl-ilp64-gcc
+   cp make.inc.mkl-icc-ilp64 make.inc.mkl-ilp64-icc
+   cp make.inc.openblas make.inc.openblas-gcc
+   cp make.inc.openblas make.inc.openblas-icc
+   cd ..
+   ln -s make.inc-examples/make.inc.$blas-$compiler make.inc
+else # maker="cmake"
+   echo "FORT = true" > make.inc
+   make generate
+   mkdir build
+   cd build
+   cmake -DGPU_TARGET=$GPU_TARGET ..
+fi
 
-ln -s make.inc-examples/make.inc.$blas-$compiler make.inc
-
-LIBDIR=-L$CUDADIR/lib64 make -j8
+make -j8
 
