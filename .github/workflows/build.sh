f3c701ba7f5bbda4357c1f60d259b92bfc0c7b04
G-Ragghianti <ragghianti@icl.utk.edu>
2022-06-25 17:25:31 -0400

    Testing CI

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -18,15 +18,16 @@ fi
 
 export CUDADIR=/usr/local/cuda
 export PATH=$PATH:$CUDADIR/bin
+export LIBRARY_PATH=$LIBRARY_PATH:$CUDADIR/lib64
 export GPU_TARGET=Volta
 
 if [ "$maker" = "make" ]; then
    cd make.inc-examples
    perl -pi -e 's/GPU_TARGET/#/' *
    cp make.inc.mkl-gcc-ilp64 make.inc.mkl-ilp64-gcc
-   cp make.inc.mkl-icc-ilp64 make.inc.mkl-ilp64-icc
+   cp make.inc.mkl-icc-ilp64 make.inc.mkl-ilp64-intel
    cp make.inc.openblas make.inc.openblas-gcc
-   cp make.inc.openblas make.inc.openblas-icc
+   cp make.inc.openblas make.inc.openblas-intel
    cd ..
    ln -s make.inc-examples/make.inc.$blas-$compiler make.inc
 else # maker="cmake"
