2be3092c1c418477bbc8223a1f8c2ca1df1c8ad1
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2022-08-23 16:31:46 -0400

    a working tester for batch band LU

--- testing/testing_zgbtrf_batched.cpp
+++ testing/testing_zgbtrf_batched.cpp
@@ -27,46 +27,106 @@
 #include "../control/magma_threadsetting.h"  // internal header
 #endif
 
-double get_LU_error(magma_int_t M, magma_int_t N,
-                    magmaDoubleComplex *A,  magma_int_t lda,
-                    magmaDoubleComplex *LU, magma_int_t *IPIV)
+double get_band_LU_error(
+            magma_int_t M, magma_int_t N,
+            magma_int_t KL, magma_int_t KU,
+            magmaDoubleComplex *AB,  magma_int_t ldab,
+            magmaDoubleComplex *LUB, magma_int_t *IPIV)
 {
+#define   A(i,j)   A[(j)*M    + (i)]
+#define  LU(i,j)  LU[(j)*M    + (i)]
+#define  AB(i,j)  AB[(j)*ldab + (i)]
+#define LUB(i,j) LUB[(j)*ldab + (i)]
+
     magma_int_t min_mn = min(M, N);
     magma_int_t ione   = 1;
     magma_int_t i, j;
     magmaDoubleComplex alpha = MAGMA_Z_ONE;
     magmaDoubleComplex beta  = MAGMA_Z_ZERO;
-    magmaDoubleComplex *L, *U;
+    magmaDoubleComplex *A, *LU, *L, *U;
     double work[1], matnorm, residual;
 
-    TESTING_CHECK( magma_zmalloc_cpu( &L, M*min_mn ));
-    TESTING_CHECK( magma_zmalloc_cpu( &U, min_mn*N ));
-    memset( L, 0, M*min_mn*sizeof(magmaDoubleComplex) );
-    memset( U, 0, min_mn*N*sizeof(magmaDoubleComplex) );
+    TESTING_CHECK( magma_zmalloc_cpu( &A,  M*N ));
+    TESTING_CHECK( magma_zmalloc_cpu( &LU, M*N ));
+    TESTING_CHECK( magma_zmalloc_cpu( &L,  M*min_mn ));
+    TESTING_CHECK( magma_zmalloc_cpu( &U,  min_mn*N ));
+    memset( A,  0, M*N*sizeof(magmaDoubleComplex) );
+    memset( LU, 0, M*N*sizeof(magmaDoubleComplex) );
+    memset( L,  0, M*min_mn*sizeof(magmaDoubleComplex) );
+    memset( U,  0, min_mn*N*sizeof(magmaDoubleComplex) );
 
-    lapackf77_zlaswp( &N, A, &lda, &ione, &min_mn, IPIV, &ione);
-    lapackf77_zlacpy( MagmaLowerStr, &M, &min_mn, LU, &lda, L, &M      );
-    lapackf77_zlacpy( MagmaUpperStr, &min_mn, &N, LU, &lda, U, &min_mn );
+    // recover A in dense form, account for extra KL super-diagonals
+    for(j = 0; j < N; j++) {
+        int col_start      = max(0, j-KU);
+        int col_end        = min(j+KL,M-1);
+        int col_length     = col_end - col_start + 1;
+        int col_start_band = KL + max(KU-j,0);
+        memcpy( &A(col_start,j), &AB(col_start_band,j), col_length * sizeof(magmaDoubleComplex));
+    }
+    // end of converting AB to dense in A
+
+    // recover LU in dense form
+    magma_int_t KV = KL + KU;
+    for(j = 0; j < N; j++) {
+        magma_int_t col_start      = max(0, j-KV);
+        magma_int_t col_end        = min(j+KL,M-1);
+        magma_int_t col_length     = col_end - col_start + 1;
+        magma_int_t col_start_band = max(KV-j,0);
+        memcpy( &LU(col_start,j), &LUB(col_start_band,j), col_length * sizeof(magmaDoubleComplex));
+    }
+
+    // swapping to recover L
+    for(j = 0; j < N-2; j++) {
+        const magma_int_t k1 = j+2;
+        const magma_int_t k2 = N;
+        lapackf77_zlaswp(&ione, &LU(0,j), &M, &k1, &k2, IPIV, &ione );
+    }
+    // end of converting LUB to dense in LU
+
+    #if 0
+    magma_int_t Mband  = KL + 1 + (KL+KU); // need extra KL for the upper factor
+    magma_int_t Nband  = N;
+    for(j = 0; j < min_mn; j++) {printf("%2d ", IPIV[j]);} printf("\n");
+    printf("AB = ");
+    magma_zprint(Mband, Nband,  AB, ldab);
+    printf("A = ");
+    magma_zprint(M, N,  A, M);
+    printf("LUB = ");
+    magma_zprint(Mband, Nband, LUB, ldab);
+    printf("LU = ");
+    magma_zprint(M, N,  LU, M);
+    #endif
+
+    lapackf77_zlaswp( &N, A, &M, &ione, &min_mn, IPIV, &ione);
+    lapackf77_zlacpy( MagmaLowerStr, &M, &min_mn, LU, &M, L, &M      );
+    lapackf77_zlacpy( MagmaUpperStr, &min_mn, &N, LU, &M, U, &min_mn );
 
     for (j=0; j < min_mn; j++)
         L[j+j*M] = MAGMA_Z_MAKE( 1., 0. );
 
-    matnorm = lapackf77_zlange("f", &M, &N, A, &lda, work);
+    matnorm = lapackf77_zlange("f", &M, &N, A, &M, work);
 
     blasf77_zgemm("N", "N", &M, &N, &min_mn,
-                  &alpha, L, &M, U, &min_mn, &beta, LU, &lda);
+                  &alpha, L, &M, U, &min_mn, &beta, LU, &M);
 
     for( j = 0; j < N; j++ ) {
         for( i = 0; i < M; i++ ) {
-            LU[i+j*lda] = MAGMA_Z_SUB( LU[i+j*lda], A[i+j*lda] );
+            LU[i+j*M] = MAGMA_Z_SUB( LU[i+j*M], A[i+j*M] );
         }
     }
-    residual = lapackf77_zlange("f", &M, &N, LU, &lda, work);
+    residual = lapackf77_zlange("f", &M, &N, LU, &M, work);
 
+    magma_free_cpu( A );
+    magma_free_cpu( LU );
     magma_free_cpu( L );
     magma_free_cpu( U );
 
     return residual / (matnorm * N);
+
+#undef A
+#undef LU
+#undef AB
+#undef LUB
 }
 
 /* ////////////////////////////////////////////////////////////////////////////
@@ -87,7 +147,7 @@ int main( int argc, char** argv)
     magma_int_t     *ipiv, *cpu_info;
     magma_int_t     *dipiv_magma, *dinfo_magma;
 
-    magma_int_t M, N, Mband, Nband, KL, KU, n2, lda, ldab, lddab, min_mn, info;
+    magma_int_t M, N, Mband, Nband, KL, KU, n2, ldab, lddab, min_mn, info;
     magma_int_t ione     = 1;
     magma_int_t ISEED[4] = {0,0,0,1};
     magma_int_t batchCount;
@@ -105,8 +165,8 @@ int main( int argc, char** argv)
 
     printf("%% Lower bandwidth (KL) = %lld\n", (long long)KL);
     printf("%% Upper bandwidth (KU) = %lld\n", (long long)KU);
-    printf("%% BatchCount   M     N    CPU Gflop/s (ms)   MAGMA Gflop/s (ms)   %s Gflop/s (ms)   ||PA-LU||/(||A||*N)\n", g_platform_str);
-    printf("%%==========================================================================================================\n");
+    printf("%% BatchCount   M     N    CPU Gflop/s (ms)   MAGMA Gflop/s (ms)   ||PA-LU||/(||A||*N)\n");
+    printf("%%=======================================================================================\n");
     for( int itest = 0; itest < opts.ntest; ++itest ) {
         for( int iter = 0; iter < opts.niter; ++iter ) {
             M = opts.msize[itest];
@@ -126,7 +186,7 @@ int main( int argc, char** argv)
             TESTING_CHECK( magma_zmalloc_cpu( &h_Amagma,  n2     ));
             TESTING_CHECK( magma_zmalloc_pinned( &h_R,  n2     ));
 
-            TESTING_CHECK( magma_zmalloc( &dA,  lddab * N * batchCount ));
+            TESTING_CHECK( magma_zmalloc( &dA,  lddab * Nband * batchCount ));
             TESTING_CHECK( magma_imalloc( &dipiv_magma,  min_mn * batchCount ));
             TESTING_CHECK( magma_imalloc( &dinfo_magma,  batchCount ));
 
@@ -146,7 +206,17 @@ int main( int argc, char** argv)
             magma_iset_pointer( dipiv_array, dipiv_magma, 1, 0, 0, min_mn, batchCount, opts.queue );
 
             magma_time = magma_sync_wtime( opts.queue );
-            info = magma_zgetrf_batched( Mband, Nband, dA_array, lddab, dipiv_array,  dinfo_magma, batchCount, opts.queue);
+            if(opts.version == 1) {
+                memcpy(h_Amagma, h_R, Mband * columns * sizeof(magmaDoubleComplex));
+                for (magma_int_t s=0; s < batchCount; s++) {
+                        magma_int_t locinfo;
+                        lapackf77_zgbtrf(&M, &N, &KL, &KU, h_Amagma + s * ldab * Nband, &ldab, ipiv + s * min_mn, &locinfo );
+                }
+                magma_zsetmatrix( Mband, columns, h_Amagma, ldab, dA, lddab, opts.queue );
+                magma_setvector( min_mn * batchCount, sizeof(magma_int_t), ipiv, 1, dipiv_magma, 1, opts.queue );
+                magma_memset_async(dinfo_magma, 0, batchCount * sizeof(magma_int_t), opts.queue);
+                info = 0;
+            }
             magma_time = magma_sync_wtime( opts.queue ) - magma_time;
             magma_perf = gflops / magma_time;
 
@@ -181,7 +251,7 @@ int main( int argc, char** argv)
                 #endif
                 for (magma_int_t s=0; s < batchCount; s++) {
                     magma_int_t locinfo;
-                    lapackf77_zgbtrf(&Mband, &Nband, &KL, &KU, h_A + s * ldab * Nband, &ldab, ipiv + s * min_mn, &locinfo );
+                    lapackf77_zgbtrf(&M, &N, &KL, &KU, h_A + s * ldab * Nband, &ldab, ipiv + s * min_mn, &locinfo );
                     if (locinfo != 0) {
                         printf("lapackf77_zgbtrf matrix %lld returned error %lld: %s.\n",
                                (long long) s, (long long) locinfo, magma_strerror( locinfo ));
@@ -225,7 +295,7 @@ int main( int argc, char** argv)
                         break;
                     }
 
-                    double err = get_LU_error( M, N, h_R + i * lda*N, lda, h_Amagma + i * lda*N, ipiv + i * min_mn);
+                    double err = get_band_LU_error(M, N, KL, KU, h_R + i * ldab*N,  ldab, h_Amagma + i * ldab*N, ipiv + i * min_mn);
                     if (std::isnan(err) || std::isinf(err)) {
                         error = err;
                         break;
