1f3fdaddfd673265a61a8baae416b74cc77a7798
Natalie Beams <nbeams@icl.utk.edu>
2022-12-20 15:46:29 -0500

    remove CUDA arch check from ported code

--- magmablas_sycl/zsymv.dp.cpp
+++ magmablas_sycl/zsymv.dp.cpp
@@ -719,16 +719,6 @@ magmablas_zsymv_work(
     magmaDoubleComplex_ptr dwork, magma_int_t lwork,
     magma_queue_t queue )
 {
-#if defined(PRECISION_z)
-    // z precision requires CUDA ARCH 2.x; call CUBLAS version instead.
-    magma_int_t arch = magma_getdevice_arch();
-    if ( arch < 200 ) {
-        //magma_zsymv( uplo, n, alpha, dA, ldda, dx, incx, beta, dy, incy );
-        //return MAGMA_SUCCESS;
-        fprintf(stderr, "%s: %s\n", __func__, "not supported on CUDA ARCH 1.x");
-        return MAGMA_ERR_NOT_SUPPORTED;
-    }
-#endif
 
     // --------------------
     // [sdc] precisions, or z precision with CUDA ARCH 2.x
