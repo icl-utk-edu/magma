dbf430c216a876b160dba5112d4003b248277423
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2024-03-25 19:15:09 +0000

    version 2.8.0

--- include/magma_types.h
+++ include/magma_types.h
@@ -513,8 +513,8 @@ float  magma_cabsf( magmaFloatComplex  x );
 
 // -----------------------------------------------------------------------------
 #define MAGMA_VERSION_MAJOR 2
-#define MAGMA_VERSION_MINOR 7
-#define MAGMA_VERSION_MICRO 2
+#define MAGMA_VERSION_MINOR 8
+#define MAGMA_VERSION_MICRO 0
 
 // stage is "svn", "beta#", "rc#" (release candidate), or blank ("") for final release
 #define MAGMA_VERSION_STAGE "svn"
