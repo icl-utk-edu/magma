305fda2bbb51d864def7bef108a6ce936728e376
G-Ragghianti <ragghianti@icl.utk.edu>
2024-09-06 15:15:23 -0400

    Testing CI

--- .github/workflows/configure.sh
+++ .github/workflows/configure.sh
@@ -2,23 +2,7 @@
 
 source .github/workflows/setup.sh
 
-if [ "$MAKER" = "make" ]; then
-   cd make.inc-examples
-   perl -pi -e 's/GPU_TARGET/#/' *
-   cp make.inc.mkl-icc       make.inc.mkl-intel
-   cp make.inc.mkl-gcc-ilp64 make.inc.mkl-ilp64-gcc
-   cp make.inc.mkl-icc-ilp64 make.inc.mkl-ilp64-intel
-   cp make.inc.openblas      make.inc.openblas-gcc
-   cp make.inc.openblas      make.inc.openblas-intel
-   cd ..
-   ln -sf make.inc-examples/make.inc.$BLAS-$COMPILER make.inc
-else # MAKER="cmake"
-   if [ "$BLAS" = "mkl-ilp64" ]; then
-      echo CMake needs to be configured to use ilp64
-      exit 1 
-   fi
-   echo -e "FORT = true\nGPU_TARGET = $GPU_TARGET\nBACKEND = $BACKEND\n" > make.inc
-   make generate
+if [ "$MAKER" = "cmake" ]; then
    mkdir -p build
    cd build
    cmake -DGPU_TARGET=$GPU_TARGET -DMAGMA_ENABLE_${BACKEND^^}=ON $OPTIONS ..
---
+++ .github/workflows/generate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source .github/workflows/setup.sh
+
+cd make.inc-examples
+cp make.inc.mkl-icc       make.inc.mkl-intel
+cp make.inc.mkl-gcc-ilp64 make.inc.mkl-ilp64-gcc
+cp make.inc.mkl-icc-ilp64 make.inc.mkl-ilp64-intel
+cp make.inc.openblas      make.inc.openblas-gcc
+cp make.inc.openblas      make.inc.openblas-intel
+cd ..
+ln -sf make.inc-examples/make.inc.$BLAS-$COMPILER make.inc
+
+make generate
