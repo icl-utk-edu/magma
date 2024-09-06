73a0703ed2cfae887b39092a8baa683c01e5c0a6
G-Ragghianti <ragghianti@icl.utk.edu>
2024-09-06 16:24:25 -0400

    Testing CI

--- .github/workflows/generate.sh
+++ .github/workflows/generate.sh
@@ -2,13 +2,9 @@
 
 source .github/workflows/setup.sh
 
-cd make.inc-examples
-cp make.inc.mkl-icc       make.inc.mkl-intel
-cp make.inc.mkl-gcc-ilp64 make.inc.mkl-ilp64-gcc
-cp make.inc.mkl-icc-ilp64 make.inc.mkl-ilp64-intel
-cp make.inc.openblas      make.inc.openblas-gcc
-cp make.inc.openblas      make.inc.openblas-intel
-cd ..
-ln -sf make.inc-examples/make.inc.$BLAS-$COMPILER make.inc
+# Temporarily create a make.inc for the generate step
+echo -e "FORT = true\nGPU_TARGET = $GPU_TARGET\nBACKEND = $BACKEND\n" > make.inc
 
-make BACKEND=$BACKEND generate
+make generate
+
+rm make.inc
