a693712fab03f3bfb8100f32d67a4411bb86baa1
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2025-12-09 13:04:15 -0500

    fix tuning for QR and UNMQR

--- control/geqrf_batched_panel_decision.h
+++ control/geqrf_batched_panel_decision.h
@@ -1,6 +1,9 @@
 #ifndef GEQRF_BATCHED_PANEL_DECISION
 #define GEQRF_BATCHED_PANEL_DECISION
 
+#define GEQRF_BATCHED_LOOKUP_TABLE_BATCH_STEP   (100)
+#define GEQRF_BATCHED_MAX_TESTED_WIDTH          (256)
+
 #ifdef MAGMA_HAVE_HIP
 #include "geqrf_panel_decision_mi100.h"
 #include "geqrf_panel_decision_mi300a.h"
