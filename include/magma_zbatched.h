21dc86958d695b50cacbc58b84461a723df20623
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2025-12-18 13:16:06 -0500

    batch Hermitian eigensolver in shared memory, plus its tuning

--- include/magma_zbatched.h
+++ include/magma_zbatched.h
@@ -66,7 +66,7 @@ magma_int_t magma_get_zgetri_batched_ntcol(magma_int_t m, magma_int_t n);
 magma_int_t magma_get_ztrsm_batched_stop_nb(magma_side_t side, magma_int_t m, magma_int_t n);
 void magma_get_zgbtrf_batched_params(magma_int_t m, magma_int_t n, magma_int_t kl, magma_int_t ku, magma_int_t *nb, magma_int_t *threads);
 magma_int_t magma_zunm2r_batched_kernel_sm_size(magma_side_t side, magma_trans_t trans, magma_int_t m, magma_int_t n, magma_int_t ib, magma_int_t nb);
-
+magma_int_t magma_get_zheevj_batched_small_nthreads(magma_int_t n);
 
 #ifdef MAGMA_REAL
 magma_int_t
