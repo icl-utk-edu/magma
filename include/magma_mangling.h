72dd6f2723117cc228dd33bd3f1484a48613c670
Joris Gillis <joris@yacoda.com>
2026-04-08 22:10:32 +0200

    Introduce MAGMA_BLAS_PREFIX

--- include/magma_mangling.h
+++ include/magma_mangling.h
@@ -19,15 +19,32 @@
  *   include/magma_*lapack.h
  *   control/magma_*f77.cpp
  */
+/* Optional BLAS symbol prefix (e.g. -DMAGMA_BLAS_PREFIX=openblas_).
+ * Two levels of indirection so the prefix macro is expanded before pasting. */
+#define _MAGMA_PASTE(a, b)  a ## b
+#define _MAGMA_EXPAND(a, b) _MAGMA_PASTE(a, b)
+
 #ifndef MAGMA_FORTRAN_NAME
     #if defined(MAGMA_GLOBAL)
         #define FORTRAN_NAME(lcname, UCNAME)  MAGMA_GLOBAL( lcname, UCNAME )
     #elif defined(ADD_)
-        #define FORTRAN_NAME(lcname, UCNAME)  lcname##_
+        #ifdef MAGMA_BLAS_PREFIX
+            #define FORTRAN_NAME(lcname, UCNAME)  _MAGMA_EXPAND(MAGMA_BLAS_PREFIX, lcname##_)
+        #else
+            #define FORTRAN_NAME(lcname, UCNAME)  lcname##_
+        #endif
     #elif defined(NOCHANGE)
-        #define FORTRAN_NAME(lcname, UCNAME)  lcname
+        #ifdef MAGMA_BLAS_PREFIX
+            #define FORTRAN_NAME(lcname, UCNAME)  _MAGMA_EXPAND(MAGMA_BLAS_PREFIX, lcname)
+        #else
+            #define FORTRAN_NAME(lcname, UCNAME)  lcname
+        #endif
     #elif defined(UPCASE)
-        #define FORTRAN_NAME(lcname, UCNAME)  UCNAME
+        #ifdef MAGMA_BLAS_PREFIX
+            #define FORTRAN_NAME(lcname, UCNAME)  _MAGMA_EXPAND(MAGMA_BLAS_PREFIX, UCNAME)
+        #else
+            #define FORTRAN_NAME(lcname, UCNAME)  UCNAME
+        #endif
     #else
         #error "One of ADD_, NOCHANGE, or UPCASE must be defined to set how Fortran functions are name mangled. For example, in MAGMA, add -DADD_ to CFLAGS, FFLAGS, etc. in make.inc. If using CMake, it defines MAGMA_GLOBAL instead."
         #define FORTRAN_NAME(lcname, UCNAME)  lcname##_error
