50de0f196350320bc3297ea3de08792955b2de44
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2026-04-10 10:29:16 -0400

    [WIP]: batch zpptrf

--- magmablas/zpptrf_batched_small.cu
+++ magmablas/zpptrf_batched_small.cu
@@ -18,12 +18,6 @@
 #define PRECISION_z
 #define DBG
 
-#ifdef MAGMA_HAVE_HIP
-#define PPTRF_MAX_THREADS (256)
-#else
-#define PPTRF_MAX_THREADS (64)
-#endif
-
 // formula for lower part access
 #define sA(i, j) sA[N*j - j*(j+1)/2 + i]
 
@@ -53,14 +47,14 @@ __device__ void print_memory(
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-template<int N>
+template<int N, int MAX_THREADS>
 __global__
 #ifdef MAGMA_HAVE_HIP
-__launch_bounds__(PPTRF_MAX_THREADS)
+__launch_bounds__(MAX_THREADS)
 #endif
 void
 zpptrf_lower_batched_small_kernel(
-        magmaDoubleComplex** dA_array,
+        magmaDoubleComplex** dAP_array,
         magma_int_t *info_array, int batchCount)
 {
     extern __shared__ magmaDoubleComplex zdata[];
@@ -72,7 +66,7 @@ zpptrf_lower_batched_small_kernel(
     const int batchid = blockIdx.x * blockDim.y + ty;
     if(batchid >= batchCount) return;
 
-    magmaDoubleComplex* dA = dA_array[batchid];
+    magmaDoubleComplex* dA = dAP_array[batchid];
     magma_int_t* info = &info_array[batchid];
 
     magmaDoubleComplex *sA = (magmaDoubleComplex*)zdata;
@@ -120,8 +114,6 @@ zpptrf_lower_batched_small_kernel(
     }
 
     // write
-    // sizeA is always divisible by N, so since we are assuming N threads,
-    // we don't need a cleanup section for the loop below
     #pragma unroll
     for(int i = 0; i < sizeA_N; i+=N){
         dA[ i + tx ] = sA[ i + tx ];
@@ -133,6 +125,129 @@ zpptrf_lower_batched_small_kernel(
 
 }
 
+////////////////////////////////////////////////////////////////////////////////
+__global__ void
+zpptrf_lower_batched_small_kernel_n(
+        magma_int_t n,
+        magmaDoubleComplex** dAP_array,
+        magma_int_t *info_array, int batchCount)
+{
+    extern __shared__ magmaDoubleComplex zdata[];
+    const int sizeA    = (n+1)*n/2;
+
+    const int tx = threadIdx.x;
+    const int ty = threadIdx.y;
+    const int  N = n; // for the sA macro defined above
+    const int batchid = blockIdx.x * blockDim.y + ty;
+    if(batchid >= batchCount) return;
+
+    magmaDoubleComplex* dA = dAP_array[batchid];
+    magma_int_t* info = &info_array[batchid];
+
+    magmaDoubleComplex *sA = (magmaDoubleComplex*)zdata;
+
+    int linfo = 0;
+    sA += ty * sizeA;
+
+    // read
+    #pragma unroll
+    for(int i = tx; i < sizeA; i+=n){
+        sA[ i ] = dA[ i ];
+    }
+    __syncthreads();
+
+    for(int j = 0; j < n; j++){
+        // update column j
+        if(tx >= j) {
+            for(int k = 0; k < j; k++) {
+                sA(tx, j) -= sA(tx, k) * MAGMA_Z_CONJ( sA(j,k) );
+            }
+        }
+        __syncthreads();
+
+        // factorize column j
+        if(tx == j) {
+            sA(j, j) = MAGMA_Z_MAKE(sqrt(MAGMA_Z_REAL(sA(j, j))), MAGMA_D_ZERO);
+        }
+        __syncthreads();
+
+        if(tx > j) {
+            sA(tx, j) *= MAGMA_Z_DIV( MAGMA_Z_ONE, sA(j, j) );
+        }
+        __syncthreads();
+    }
+
+    if(tx == 0){
+        (*info) = (magma_int_t)( linfo );
+    }
+
+    // write
+    for(int i = tx; i < sizeA; i+=n){
+        dA[ i ] = sA[ i ];
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+template<int N, int MAX_THREADS>
+magma_int_t
+zpptrf_lower_batched_small_kernel_driver(
+    magma_uplo_t uplo,
+    magmaDoubleComplex** dAP_array, magma_int_t* info_array,
+    magma_int_t batchCount, magma_queue_t queue )
+{
+    magma_device_t device;
+    magma_getdevice( &device );
+    magma_int_t arginfo = 0;
+
+    if(uplo != MagmaLower)
+        arginfo = -1;
+    else if( (N < 0) || ( N > 64 ) )
+        arginfo = -2;
+    else if ( batchCount < 0 )
+        arginfo = -5;
+
+    if (arginfo != 0) {
+        return arginfo;
+    }
+
+    if( N == 0 || batchCount == 0 ) return 0;
+
+    #ifdef MAGMA_HAVE_HIP
+    magma_int_t ntcol = min(1, MAX_THREADS / 64);
+    #else
+    magma_int_t ntcol = min(1, MAX_THREADS / 32);
+    #endif
+
+    magma_int_t shmem = ntcol * (N * (N+1) / 2) * sizeof(magmaDoubleComplex);
+    magma_int_t gridx = magma_ceildiv(batchCount, ntcol);
+    dim3 threads(N, ntcol, 1);
+    dim3 grid(gridx, 1, 1);
+
+    int shmem_max = 0;
+    #if CUDA_VERSION >= 9000
+    cudaDeviceGetAttribute (&shmem_max, cudaDevAttrMaxSharedMemoryPerBlockOptin, device);
+    if (shmem <= shmem_max) {
+        cudaFuncSetAttribute(zpptrf_lower_batched_small_kernel<N, MAX_THREADS>, cudaFuncAttributeMaxDynamicSharedMemorySize, shmem);
+    }
+    #else
+    cudaDeviceGetAttribute (&shmem_max, cudaDevAttrMaxSharedMemoryPerBlock, device);
+    #endif    // CUDA_VERSION >= 9000
+
+    if ( shmem > shmem_max ) {
+        arginfo = -100;;
+    }
+
+    void *kernel_args[] = {&dAP_array, &info_array, &batchCount};
+    cudaError_t e = cudaLaunchKernel((void*)zpptrf_lower_batched_small_kernel<N, MAX_THREADS>, grid, threads, kernel_args, shmem, queue->cuda_stream());
+
+    if( e != cudaSuccess ) {
+        //printf("error in %s : failed to launch kernel %s\n", __func__, cudaGetErrorString(e));
+        arginfo = -100;
+    }
+
+
+    return arginfo;
+}
 /***************************************************************************//**
     Purpose
     -------
@@ -147,31 +262,16 @@ zpptrf_lower_batched_small_kernel(
             The size of each matrix A.  N >= 0.
 
     @param[in,out]
-    dA_array    Array of pointers, dimension (batchCount).
+    dAP_array    Array of pointers, dimension (batchCount).
             Each is a COMPLEX_16 array on the GPU, dimension (LDDA,N).
-            On entry, each pointer is an M-by-N matrix to be factored.
-            On exit, the factors L and U from the factorization
-            A = P*L*U; the unit diagonal elements of L are not stored.
-
-    @param[in]
-    ldda    INTEGER
-            The leading dimension of each array A.  LDDA >= max(1,M).
-
-    @param[out]
-    ipiv_array  Array of pointers, dimension (batchCount), for corresponding matrices.
-            Each is an INTEGER array, dimension (min(M,N))
-            The pivot indices; for 1 <= i <= min(M,N), row i of the
-            matrix was interchanged with row IPIV(i).
+            On entry, each pointer is
 
     @param[out]
     info_array  Array of INTEGERs, dimension (batchCount), for corresponding matrices.
       -     = 0:  successful exit
       -     < 0:  if INFO = -i, the i-th argument had an illegal value
                   or another error occured, such as memory allocation failed.
-      -     > 0:  if INFO = i, U(i,i) is exactly zero. The factorization
-                  has been completed, but the factor U is exactly
-                  singular, and division by zero will occur if it is used
-                  to solve a system of equations.
+      -     > 0:  if INFO = i,
 
     @param[in]
     batchCount  INTEGER
@@ -186,14 +286,14 @@ zpptrf_lower_batched_small_kernel(
 extern "C" magma_int_t
 magma_zpptrf_batched_small(
     magma_uplo_t uplo, magma_int_t n,
-    magmaDoubleComplex** dA_array, magma_int_t* info_array,
+    magmaDoubleComplex** dAP_array, magma_int_t* info_array,
     magma_int_t batchCount, magma_queue_t queue )
 {
     magma_int_t arginfo = 0;
 
     if(uplo != MagmaLower)
         arginfo = -1;
-    else if( (n < 0) || ( n > 32 ) )
+    else if( (n < 0) || ( n > 64 ) )
         arginfo = -2;
     else if ( batchCount < 0 )
         arginfo = -5;
@@ -205,54 +305,52 @@ magma_zpptrf_batched_small(
 
     if( n == 0 || batchCount == 0 ) return 0;
 
-    #ifdef MAGMA_HAVE_HIP
-    const magma_int_t ntcol = PPTRF_MAX_THREADS / 64;;
-    #else
-    const magma_int_t ntcol = PPTRF_MAX_THREADS / 32;
-    #endif
-    magma_int_t shmem  = ntcol * n * (n+1) * sizeof(magmaDoubleComplex);
-    dim3 threads(n, ntcol, 1);
-    const magma_int_t gridx = magma_ceildiv(batchCount, ntcol);
-    dim3 grid(gridx, 1, 1);
-
+    arginfo = -100;
     switch(n){
-        #if 0
-        case  8: zpptrf_lower_batched_small_kernel< 8><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        #else
-        case  1: zpptrf_lower_batched_small_kernel< 1><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case  2: zpptrf_lower_batched_small_kernel< 2><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case  3: zpptrf_lower_batched_small_kernel< 3><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case  4: zpptrf_lower_batched_small_kernel< 4><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case  5: zpptrf_lower_batched_small_kernel< 5><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case  6: zpptrf_lower_batched_small_kernel< 6><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case  7: zpptrf_lower_batched_small_kernel< 7><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case  8: zpptrf_lower_batched_small_kernel< 8><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case  9: zpptrf_lower_batched_small_kernel< 9><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case 10: zpptrf_lower_batched_small_kernel<10><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case 11: zpptrf_lower_batched_small_kernel<11><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case 12: zpptrf_lower_batched_small_kernel<12><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case 13: zpptrf_lower_batched_small_kernel<13><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case 14: zpptrf_lower_batched_small_kernel<14><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case 15: zpptrf_lower_batched_small_kernel<15><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case 16: zpptrf_lower_batched_small_kernel<16><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case 17: zpptrf_lower_batched_small_kernel<17><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case 18: zpptrf_lower_batched_small_kernel<18><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case 19: zpptrf_lower_batched_small_kernel<19><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case 20: zpptrf_lower_batched_small_kernel<20><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case 21: zpptrf_lower_batched_small_kernel<21><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case 22: zpptrf_lower_batched_small_kernel<22><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case 23: zpptrf_lower_batched_small_kernel<23><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case 24: zpptrf_lower_batched_small_kernel<24><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case 25: zpptrf_lower_batched_small_kernel<25><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case 26: zpptrf_lower_batched_small_kernel<26><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case 27: zpptrf_lower_batched_small_kernel<27><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case 28: zpptrf_lower_batched_small_kernel<28><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case 29: zpptrf_lower_batched_small_kernel<29><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case 30: zpptrf_lower_batched_small_kernel<30><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case 31: zpptrf_lower_batched_small_kernel<31><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        case 32: zpptrf_lower_batched_small_kernel<32><<<grid, threads, shmem, queue->cuda_stream()>>>(dA_array, info_array, batchCount); break;
-        #endif
-        default: fprintf(stderr, "MAGMA: error in *pptrf_batched_small, unsupported size '%lld'\n", (long long)n);
+        case  1: arginfo = zpptrf_lower_batched_small_kernel_driver< 1, 128>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case  2: arginfo = zpptrf_lower_batched_small_kernel_driver< 2, 128>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case  3: arginfo = zpptrf_lower_batched_small_kernel_driver< 3, 128>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case  4: arginfo = zpptrf_lower_batched_small_kernel_driver< 4, 128>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case  5: arginfo = zpptrf_lower_batched_small_kernel_driver< 5, 128>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case  6: arginfo = zpptrf_lower_batched_small_kernel_driver< 6, 128>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case  7: arginfo = zpptrf_lower_batched_small_kernel_driver< 7, 128>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case  8: arginfo = zpptrf_lower_batched_small_kernel_driver< 8, 128>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case  9: arginfo = zpptrf_lower_batched_small_kernel_driver< 9,  64>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case 10: arginfo = zpptrf_lower_batched_small_kernel_driver<10,  64>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case 11: arginfo = zpptrf_lower_batched_small_kernel_driver<11,  64>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case 12: arginfo = zpptrf_lower_batched_small_kernel_driver<12,  64>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case 13: arginfo = zpptrf_lower_batched_small_kernel_driver<13,  64>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case 14: arginfo = zpptrf_lower_batched_small_kernel_driver<14,  64>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case 15: arginfo = zpptrf_lower_batched_small_kernel_driver<15,  64>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case 16: arginfo = zpptrf_lower_batched_small_kernel_driver<16,  64>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case 17: arginfo = zpptrf_lower_batched_small_kernel_driver<17,  64>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case 18: arginfo = zpptrf_lower_batched_small_kernel_driver<18,  64>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case 19: arginfo = zpptrf_lower_batched_small_kernel_driver<19,  64>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case 20: arginfo = zpptrf_lower_batched_small_kernel_driver<20,  64>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case 21: arginfo = zpptrf_lower_batched_small_kernel_driver<21,  64>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case 22: arginfo = zpptrf_lower_batched_small_kernel_driver<22,  64>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case 23: arginfo = zpptrf_lower_batched_small_kernel_driver<23,  64>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case 24: arginfo = zpptrf_lower_batched_small_kernel_driver<24,  64>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case 25: arginfo = zpptrf_lower_batched_small_kernel_driver<25,  64>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case 26: arginfo = zpptrf_lower_batched_small_kernel_driver<26,  64>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case 27: arginfo = zpptrf_lower_batched_small_kernel_driver<27,  64>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case 28: arginfo = zpptrf_lower_batched_small_kernel_driver<28,  64>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case 29: arginfo = zpptrf_lower_batched_small_kernel_driver<29,  64>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case 30: arginfo = zpptrf_lower_batched_small_kernel_driver<30,  64>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case 31: arginfo = zpptrf_lower_batched_small_kernel_driver<31,  64>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        case 32: arginfo = zpptrf_lower_batched_small_kernel_driver<32,  64>(uplo, dAP_array, info_array, batchCount, queue ); break;
+        default: arginfo = -100;
     }
+
+    if(arginfo != 0) {
+        magma_int_t ntcol = 1;
+        magma_int_t shmem = ntcol * (n * (n+1) / 2) * sizeof(magmaDoubleComplex);
+        magma_int_t gridx = magma_ceildiv(batchCount, ntcol);
+        dim3 threads(n, ntcol, 1);
+        dim3 grid(gridx, 1, 1);
+
+        zpptrf_lower_batched_small_kernel_n<<<grid, threads, shmem, queue->cuda_stream()>>>(n, dAP_array, info_array, batchCount);
+    }
+
     return arginfo;
 }
